7981d8ebe8fd284a628af608548391d315874d22
Adamos Porfiro <adamos.contato@gmail.com>
2023-05-08 17:35:38 -0300

    Ex001_addpastas

---
+++ 100_exercícios.py/ex001.py
@@ -0,0 +1,14 @@
+'''
+Exercício - 1
+
+Crie um programa que escreva na tela "Olá,mundo" na tela
+
+'''
+
+# Alternativa - 1
+
+msg = '"Olá, mundo"'
+print(msg)
+
+# Alternativa - 2
+print('"Olá, mundo"')
