266349fa5bfbf98073ad15800cc6f702a7055c0c
Adamos Porfiro <adamos.contato@gmail.com>
2023-05-08 17:52:39 -0300

    ex002.py

---
+++ 100_exercícios.py/ex002.py
@@ -0,0 +1,25 @@
+'''
+
+Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+
+=== Metodo 5 Q's ===
+
+1 - Quais são os dados de entrada?
+-> input Nome
+
+2 - O que devo fazer com esses dados?
+-> Ler os dados de entrada e exibir na tela um mensagem de boas-vindas para o usuario
+
+3 - Quais são as restrições desse programa?
+-> Nenhuma
+
+4 - Qual é o resultado esperado?
+-> Que o programa leia os dados digitados e exiba na tela uma mensagem de boas-vindas para o usuario;
+
+5 - Qual a sequência de passos para se alcançar o resultado?
+#1 input nome
+#2 print Olá, nome seja bem vindo!
+
+'''
+nome = input('Qual o seu nome? ')
+print('Olá',nome+', Seja bem vindo!!!')
