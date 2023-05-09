cd4d62276d42cb284fd9aee41d88da9bc95cc743
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2023-05-08 21:53:49 -0300

    add_format

--- Tipos_primitivos.py/Desafio_2.py
+++ Tipos_primitivos.py/Desafio_2.py
@@ -1,5 +1,5 @@
 # Faça um programa que  leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as  informações possiveis sobre ele.
-
+'''
 print('===== Desafio 2 =====')
 n1 = input('Digite algo: ')
 print (type(n1))
@@ -13,7 +13,12 @@ print ('Os dados digitados são todos digitos?', n1.isdigit())
 print ('Os dados digitados é um identificador valido?', n1.isidentifier()) # _valido = 123 ou valido = 123;
 print ('Os dados digitados usam letras minusculas?', n1.islower())
 print ('Os dados digitados usam letras maiusculas?', n1.isupper())
-print ('Os dados digitados foram escritos um titulo?', n1.istitle()) # O Pequeno Príncipe_observe as letras maiusculas;
+print ('Os dados digitados foram escritos um titulo (Capitalizado)?', n1.istitle()) # O Pequeno Príncipe_observe as letras maiusculas; (Capitalizado)
 print ('Os dados digitados são caracteres imprimiveis?', n1.isprintable())
-
+'''
+# Utilizando função format
+n1 = input('Digite algo: ')
+print ('{} é alfabeticos?'.format(n1), n1.isalpha())
+print('{} é numerico?'.format(n1), n1.isalnum())
+print('{} é um alfanumerico?'.format(n1), n1.isalnum())
 
