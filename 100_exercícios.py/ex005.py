5ead63eec074f72037ab701b2bc54e8b6f5ac330
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2023-05-10 21:26:01 -0300

    Change_ex005

--- 100_exercícios.py/ex005.py
+++ 100_exercícios.py/ex005.py
@@ -27,7 +27,7 @@ Metodo 5 Q's
 
 '''
 
-print ('=' * 6,'Exercicio 5','='*6)
-
-n1 = int(input('\nDigite um numero inteiro para exibir o seu sucessor e antecessor\n'))
-print('\nO sucessor do número {} é: {:-^15}'.format(n1, n1 + 1), '\nO antecessor do número  {} é: {:-^15}'.format(n1, n1 - 1))
+print ('=' * 25, 'Exercicio 5' , '=' * 25)
+n1 = int(input('\nDigite um numero inteiro para exibir o seu sucessor e antecessor\nnumero:'))
+print('\nO sucessor do número {} é: {}\nO antecessor do número  {} é: {}'.format(n1, (n1 + 1), n1, (n1 - 1)))
+print('=' * 63)
