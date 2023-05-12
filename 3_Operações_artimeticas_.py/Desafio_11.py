da55529ce758663ec644d82e7c9f632f7c37f90f
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2023-05-12 20:54:26 -0300

    Add_desafio11_ex015.py

---
+++ 3_Operações_artimeticas_.py/Desafio_11.py
@@ -0,0 +1,8 @@
+'''
+===== Desafio 11 =====
+
+Escreva um programa que  pergunte a quantidade de km percorridos por um carro alugado e a quantidade de dias pelos quais ele
+foi alugado. Calcule o preço a pagar, sabendo que o carro custa R$ 60.00 por dia R$ 0.15 centavos por km rodado.
+
+
+'''
