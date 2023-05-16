8cc5cd9ef36391a4353ff6318ecaf272e9343ed5
Adamos Porfiro <adamos.contato@gmail.com>
2023-05-16 18:19:13 -0300

    Add_ex021_Desafio5.py

--- 100_exercícios.py/ex021.py
+++ 100_exercícios.py/ex021.py
@@ -1,5 +1,5 @@
 '''
-===== Deafio 5 =====
+===== Exercicio 21 =====
 
 O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
 Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
