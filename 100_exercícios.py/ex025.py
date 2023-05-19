3227790686920586e5850761a59a291a05e7f6cc
Adamos Porfiro <adamos.contato@gmail.com>
2023-05-19 13:52:59 -0300

    Change_code.py

--- 100_exercícios.py/ex025.py
+++ 100_exercícios.py/ex025.py
@@ -1,9 +1,15 @@
 '''
 Crie um programa que leie o nome de uma pessoa e diga se ela tem "Silva" no nome
-'''
+
 n = str(input('Nome completo: '))
 n1 = n.lower()
 if("silva" in n1):
     print('Sim, o seu nome possui silva')
 else:
     print('Não, o seu nome não possui silva')
+    
+'''
+
+n = str(input('Nome completo: ')).strip()
+min = n.lower()
+print('\nAnalisando nome: {}...\nPossui silva no nome: {}'.format(n, "silva" in min))
