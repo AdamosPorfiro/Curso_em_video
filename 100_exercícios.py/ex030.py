e9500e9038107acb35301286cd63a78dce0dcf4c
Adamos Porfiro <adamos.contato@gmail.com>
2023-05-20 12:01:05 -0300

    add_changelle_3_and_ex030.py

--- 100_exercícios.py/ex030.py
+++ 100_exercícios.py/ex030.py
@@ -16,7 +16,8 @@ Crie um programa que leia um numero inteiro e mostre na tela se ele é PAR ou IM
 
 5 - Quais são os passos necessarios para se alcançar o resultado esperado?
 #1 input numero_inteiro
-#2 if numero_inteiro == 0:
+#2 variavel1 = numero_inteiro % 2
+#3 if variavel1 == 0:
         print O numero_inteiro é PAR
     else
         print O numero_inteiro é IMPAR
@@ -25,7 +26,7 @@ print('\n'+'=' * 28,'\n','\nCALCULADORA DE IMPAR OU PAR\n')
 n = int(input('Informe um numero inteiro\nNumero: '))
 r = n % 2
 if r == 0:
-    print('\nO numero {} é PAR.'.format(n))
+    print('O numero {} é PAR.'.format(n))
 else:
-    print('\nO numero {} é IMPAR.'.format(n))
+    print('O numero {} é IMPAR'.format(n))
 print('=' * 28)
