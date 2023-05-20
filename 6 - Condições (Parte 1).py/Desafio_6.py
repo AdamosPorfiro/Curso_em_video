a0df026544bb611e870a273fbec1dd676f814361
Adamos Porfiro <adamos.contato@gmail.com>
2023-05-20 18:09:02 -0300

    Add_code_changelle_6.py

--- 6 - Condições (Parte 1).py/Desafio_6.py
+++ 6 - Condições (Parte 1).py/Desafio_6.py
@@ -1,3 +1,20 @@
 '''
 Faça um programa que leia três números e mostre qual é o maior e qual é o menor.
+
+1 - Quais são os dados de entrada?
+- input n1,n2,n3
+
+2 - O que devo fazer com esses dados?
+- Exibir qual é o maior e qual é o menor
+
+3 - Qual é a restrição desse problema?
+- São apenas 3 numeros;
+- Apenas 2 serão exibidos na tela
+
+4 - Qual é o resultado esperado?
+- Exiba na tela qual é o maior numero e qual é o menor numero.
+
+5 - Quais são os passos para se alcançar o resultado?
+#1 input n1, n2, n3
+2# 
 '''
