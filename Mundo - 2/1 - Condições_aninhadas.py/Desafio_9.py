5f0a3cc7df46fb3e2393fa8ea586cb2c39945761
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2023-05-24 21:40:46 -0300

    change_statement.py

--- Mundo - 2/1 - Condições_aninhadas.py/Desafio_9.py
+++ Mundo - 2/1 - Condições_aninhadas.py/Desafio_9.py
@@ -1,5 +1,5 @@
 '''
-Elabore um origrama que calcule o valor a ser pago por um produto, considerando o seu
+Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu
 preço normal e condição de pagamento:
 
 - A vista dinheiro / cheque: 10% de desconto;
