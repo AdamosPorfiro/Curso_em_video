d9b625e97fcf2c6d033d0746ad783b6cf3cea403
Adamos Porfiro <adamos.contato@gmail.com>
2023-05-29 18:10:20 -0300

    Add_code.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_1.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_1.py
@@ -2,6 +2,13 @@
 Faça um programa que mostre na tela a contagem regressiva para o estouro de fogos de artificios, indo de 10 até 0,
 com uma pausa de 1 segundo entre eles
 
-
+Analise: 
+For = Para fazer a contagem de 10 até 0;
+modulo TIME a função SLEEP para fazer uma pausa de 1 seg;
 
 '''
+print('\n'+'{:=^10}'.format('Contador 10 á 0'))
+from time import sleep
+for c in range(10,0,-1):
+    print(c)
+    sleep(1)
