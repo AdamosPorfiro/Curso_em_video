2170b548df4906e7d1fed3232a559a6e3e9e5f48
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-01 18:11:20 -0300

    Change_code_add_def.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_11.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_11.py
@@ -5,43 +5,43 @@ Desenvolva um programa que leia, nome, idade e sexo de quatro pessoas. No final 
 - Qual é o nome do homem mais velho;
 - E quantas mulheres tem menos de 20 anos;
 '''
-nome = []
-idade = []
-sexo = []
-media = 0
-maior_idade = 0
-homem_mais_velho = ''
-qtd_mulher_menor_20 = 0
-for c in range(4):
-    n = str(input('Qual o seu nome?\nNome: '))
-    i = int(input('Qual a sua idade?\nIdade: '))
-    s = int(input('Qual o seu sexo? 1 - Masculino 2 - Feminino:\nEscolha: '))
-    nome.append(n)
-    idade.append(i)
-    if s == 1:
-        s = 'Masculino'
-        sexo.append(s)
-        if i>maior_idade:
-            homem_mais_velho = n
-            maior_idade = i
-    
-    elif s == 2:
-        s = 'Feminino'
-        sexo.append(s)
-        if i<20:
-            qtd_mulher_menor_20 = qtd_mulher_menor_20+1
-    else:
-        print('Ops, informação errada tente novamente!')
-        break
-    #Calcular média 
-for item in idade:
-    media = (media + item)
-print('''\nA média de idade é de:{:.0f}
-      \nO homem mais velho é: {}
-      \nQuantidade de mulheres menores que 20 anos é: {}'''
-      .format(media/4,homem_mais_velho,qtd_mulher_menor_20
-    ))
-
+def processar_dados():
+    nome = []
+    idade = []
+    sexo = []
+    media = 0
+    maior_idade = 0
+    homem_mais_velho = ''
+    qtd_mulher_menor_20 = 0
+    for c in range(4):
+        n = str(input('Qual o seu nome?\nNome: '))
+        i = int(input('Qual a sua idade?\nIdade: '))
+        s = int(input('Qual o seu sexo? 1 - Masculino 2 - Feminino:\nEscolha: '))
+        nome.append(n)
+        idade.append(i)
+        if i == i:
+            media = (media+i)
+        if s == 1:
+            s = 'Masculino'
+            sexo.append(s)
+            if i>maior_idade:
+                homem_mais_velho = n
+                maior_idade = i
+                    
+        elif s == 2:
+            s = 'Feminino'
+            sexo.append(s)
+            if i<20:
+                qtd_mulher_menor_20 = qtd_mulher_menor_20+1
+        elif s != 1 and s != 2:
+            print('Algo deu errado, tente novamente!')
+            return
+    print('''\nA média de idade é de:{:.0f}
+        \nO homem mais velho é: {}
+        \nQuantidade de mulheres menores que 20 anos é: {}'''
+        .format(media/4,homem_mais_velho,qtd_mulher_menor_20
+        ))
+processar_dados()
 
 
 
