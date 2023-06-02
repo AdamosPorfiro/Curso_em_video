7e40e828ffc199d892fe1c8165f8e0ec7d6b7d70
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-02 16:01:25 -0300

    Remove_last_print.py

--- Mundo - 2/100_exercícios.py/ex049.py
+++ Mundo - 2/100_exercícios.py/ex049.py
@@ -5,4 +5,3 @@ print('\n'+'{: ^5}{}'.format('','Tabuada'))
 n1 = int(input("Informe um numero: "))
 for c in range(1, 11):
     print("{:} x {:2} = {:3}".format(n1, c, n1 * c))
-print('{:=^20}'.format(''))
