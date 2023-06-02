0c3d4480a79e7ca96ca542d97fe51b0c54ba38b1
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-02 11:50:57 -0300

    change.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_4.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_4.py
@@ -4,5 +4,5 @@ Refaça o desafio 009, mostrando a tabuada de um numero que o usuario escolher, 
 print('\n'+'{: ^5}{}'.format('','Tabuada'))
 n1 = int(input("Informe um numero: "))
 for c in range(1, 11):
-    print("{:} x {:2} = {:3}".format(n1, c, n1 * c))
+    print("{} x {:2} = {:3}".format(n1, c, n1 * c))
 print('{:=^20}'.format(''))
