86e93f5691c2d700c2c1c29c78243301ec4eda9a
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-02 16:27:49 -0300

    change_code.py

--- Mundo - 2/100_exercícios.py/ex050.py
+++ Mundo - 2/100_exercícios.py/ex050.py
@@ -6,8 +6,10 @@ impar desconsidere-o
 '''
 print('{:=^30}\n{:^6}{}'.format('','','Soma de pares'))
 soma = 0 # Inicio uma variavel 0 para armazenar a soma
+cont = 0
 for c in range(6): # Repetição dos comandos 6 vezes
     n1 = int(input('Informe um numero inteiro: ')) # Receber dados do usuario
     if n1%2 == 0: # Verificar se o numero é par
         soma += n1 # Somar e armazenar na variavel soma o resultado
-print('A soma dos numero pares é: {}\n{:=^30}'.format(soma,''))
+        cont += 1
+print('A soma de {} pares é: {}\n{:=^30}'.format(soma,''))
--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_5.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_5.py
@@ -8,11 +8,13 @@ impar desconsidere-o
 print('Soma dos valores pares')
 # Receber os dados
 soma = 0 # Iniciar uma variavel 0, que vai armazenar a soma dos elementos pares;
-for c in range(6): # Faço um contador for que vai repetir 6 vezes;
-     n1 = int(input('Informe um numero: ')) # Pergunto para o usuario o numero desejado;
+cont = 0
+for c in range(1, 7): # Faço um contador for que vai repetir 6 vezes;
+     n1 = int(input('Informe o {} º valor: '.format(c))) # Pergunto para o usuario o numero desejado;
      if n1%2==0: # Monto uma estrutura de condição que vai verificar se o numero informado é par;
         soma += n1 # Chamo a variavel soma que vai somar o valor par digitado com o que estiver armazenado. Ex: 0+2=2 na segunda vez vai ser 2+4=6 e assim por diante;
-print('A soma dos numeros pares é: {}'.format(soma)) # Exibo para o usuario o resultado da soma de todos os valores;
+        cont += 1
+print('A soma de {} pares é: {}'.format(cont,soma)) # Exibo para o usuario o resultado da soma de todos os valores;
         
         
         
