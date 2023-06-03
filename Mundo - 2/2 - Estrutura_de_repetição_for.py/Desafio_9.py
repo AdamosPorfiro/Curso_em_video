c916a89bd3ce892177dc1943d68bbfd033042be5
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-03 16:08:51 -0300

    change_struct_code.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_9.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_9.py
@@ -4,43 +4,37 @@ ainda não atingiram maioridade e quantas já são maiores.
 
 Ex: 18 anos
 
-Analise: 
-- Usamos um for para repetir o input 7 vezes;
-- Podemos montar uma lista;
-- Usar função FOR para passar por cada elemento da lista e verificar quem atingiu a maioridade ou não;
-- E no fim usar uma condição para exibir quantas são maiores e quantas são menores
-
 Quais são os dados de entrada?
-input idade 7x
+input ano_nascimento
 
 O que devo fazer com esses dados?
-Armazena-los em uma lista para verificar em seguida quem atingiu a maioridade ou não;
+Calcular a idade de cada um e armazena-los em um lista em seguida compara-lás para descobrir quem é maioro ou menor
 
 Qual a restrição desse problema?
 Maioridade = 18+ anos;
 Menores = 17- anos;
 
 Qual é o resultado esperado?
-Ler as 7 idades e exibir quantas pessoas ainda não atingiram a maioridade e quantas são menores
+Ler as 7 datas, calcular e exibir quantas pessoas ainda não atingiram a maioridade e quantas são menores
 
 Quais são os passos para se alcançar o resultado esperado?
-    1 lista[]
-    2 for 7 input idade
-    3   idade = lista
-    4 for item in lista:
-    5   if item >= 18:
-            maior = []
-            item = maior
-        elif(if) item <= 17:    
-            menor = []
-            item = menor
-    6 for qtd_maior in maior:
-        if maior:
-            print(qtd_maior)
-    7 for qtd_menor in menor:
-        if menor:
-            print(qtd_menor)
-'''
+    1 import datetime / date 
+    2 ano[]
+    3 for input ano_nascimento
+    4 idade[]
+    5 data = date.today().year
+    5 for i in ano:
+        i = i -= data
+        i.append(idade)
+    6 maior = []
+    7 menor = []
+    8 for s in idade:
+    9   if s>=18:
+            s.append(maior)
+    10  else:
+            s.append(menor)
+    11 print Possui {} maior e {} menores. format len(maior), len(menor)      
+
 
 lista = [] # Cria uma lista vazia;
 for idade in range(7): # Repete o input 7x e armazena dentro de lista;
@@ -54,3 +48,23 @@ for item in lista: # Crio outro for para passar na primeira lista que criamos
     elif item<18: # Caso seja menor que 18 ele vai inserir o elemento na lista menor
         menor.append(item)
 print('\nPossui {} maiores de idade\nPossui {} menores de idade'.format(len(maior), len(menor))) # Usamos len para ler a quantidade de elemento dentro de cada lista e exibimos;
+'''
+from datetime import date 
+Ano = []
+maior = []
+menor = []
+ano_a = date.today().year
+for item in range(1, 8):
+    i = int(input('informe o {}° Ano de nascimento: '.format(item)))
+    Ano.append(i)
+for item in Ano:
+    Ano = ano_a - item
+    if Ano>=18:
+        maior.append(Ano)
+    else:
+        menor.append(Ano)
+print('\nQuantidade de pessoas maiores de idade: {}\nQuantidade de pessoas menores de idade: {}'.format(len(maior), len(menor)))
+
+        
+
+
