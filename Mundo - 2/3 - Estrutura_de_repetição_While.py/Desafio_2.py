916a2ab802c1a428407f670e67ebf046228fe12e
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-05 18:08:41 -0300

    added_lógic.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_2.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_2.py
@@ -2,4 +2,39 @@
 Melhore o jogo do DESAFIO  onde o computador vai pensar em um numero entre 0 e 10.
 Só que agora o jogador vai tentar adivinhar até, acertar, mostrando no final quantos
 palpites foram necessarios para vencer.
+
+Metodo dos 5 q's
+
+1 - Quais são os dados de entrada?
+input computador
+input palpite_usuario
+
+2 - O que devo fazer com esses dados?
+Deve compara-lo com os dados armazenados pelo computador e exibir na tela se o usuario
+acertou o palpite
+
+3 - Quais são as restrições do problema?
+ Computador vai pensar em um numero apenas de 1 a 10
+ Jogador vai dar um palpite até acertar
+
+4 - Qual é o resultado esperado?
+O computador deve pensar em um numero armazenar, o usuario deve dar um palpite sobre
+o numero pensado pelo computador, caso ele acerte uma mensagem de ganhou irá aparecer e
+se ele deseja continuar ou não o jogo, caso erre uma mensagem de perdeu irá aparecer e
+se ele deseja continuar ou não o jogo. 
+
+5 - Quais são os passos para se alcançar o resultado esperado?
+Vamos importar a biblioteca random e função radint;
+usaremos radint para que o computador selecione um numero entre 0 e 10
+Esse numero será armazenado em uma variavel;
+contador_de_palpite = 0
+continuar = ''
+while palpite_usuario != n
+    input usuario
+    if palpite_usuario != n
+        contador_de_palpite += 1
+        continuar = str(input('Você errou, deseja tenta novamente? S/N: !')
+
+
 '''
+
