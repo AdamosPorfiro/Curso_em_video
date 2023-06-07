6b37ac1d12b12138093e20515187bce60e6ad038
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-07 14:23:55 -0300

    added_code.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_3.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_3.py
@@ -7,3 +7,5 @@ Crie um programa que leia dois valores e mostre um menu na tela:
 [5] Sair do programa
  Seu programa deverá realizar a operação solicitada em cada caso.
 '''
+n1 = float(input('Informe o primeiro numero: ')) 
+n2 = float(input('Informe o segundo numero: '))
