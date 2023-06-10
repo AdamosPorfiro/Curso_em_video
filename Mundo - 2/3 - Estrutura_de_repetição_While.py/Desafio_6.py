d4e8f540f2432d50e0b2788035c185f3a9e270bc
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-10 15:39:22 -0300

    Added_changelle_5_and_ex061.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_6.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_6.py
@@ -1,4 +1,14 @@
 '''
-Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos.
+Melhore o DESAFIO 51, perguntando para o usuário se ele quer mostrar mais alguns termos.
 O programa encerra quando ele disser que quer mostrar 0 termos.
 '''
+print('{:=^60}\n{:^18}PROGRESSÂO ARITMÉTICA\n{:=^60}'.format('','',''))
+n1 = int(input('{:^20}Informe o termo: '.format('')))
+n2 = int(input('{:^20}Informe o razão: '.format(''))) 
+contador = 0
+print('\n{:^10}Os 10 primeiros termos dessa progressão é:\n'.format('')) # EXIBIR
+while contador < 10:
+    termo = n1 + contador*2 
+    print('➝ {:^4}'.format(termo),end='')
+    contador += 1
+print('\n{:=^60}'.format(''))
