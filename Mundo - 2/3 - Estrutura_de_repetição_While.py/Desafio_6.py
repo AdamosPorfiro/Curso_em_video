7604d32e020e9b67944619a547a88c6aebbf3189
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-10 17:29:55 -0300

    added_code.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_6.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_6.py
@@ -3,12 +3,20 @@ Melhore o DESAFIO 51, perguntando para o usuário se ele quer mostrar mais algun
 O programa encerra quando ele disser que quer mostrar 0 termos.
 '''
 print('{:=^60}\n{:^18}PROGRESSÂO ARITMÉTICA\n{:=^60}'.format('','',''))
-n1 = int(input('{:^20}Informe o termo: '.format('')))
-n2 = int(input('{:^20}Informe o razão: '.format(''))) 
-contador = 0
-print('\n{:^10}Os 10 primeiros termos dessa progressão é:\n'.format('')) # EXIBIR
-while contador < 10:
-    termo = n1 + contador*2 
-    print('➝ {:^4}'.format(termo),end='')
-    contador += 1
-print('\n{:=^60}'.format(''))
+reiniciar = 1
+while reiniciar == 1:
+    n1 = int(input('{:^20}Informe o termo: '.format('')))
+    n2 = int(input('{:^20}Informe o razão: '.format(''))) 
+    contador = 0
+    print('\n{:^10}Os 10 primeiros termos dessa progressão é:\n'.format(''))
+    while contador < 10:
+        termo = n1 + contador*2 
+        print('➝ {:^4}'.format(termo),end='')
+        contador += 1
+    reiniciar = int(input(
+    '''
+                \nDeseja ver mais termos? 
+                        [0] - não  
+                        [1] - sim
+                        Escolha: '''))
+    print('\n\n{:=^60}'.format(''))
