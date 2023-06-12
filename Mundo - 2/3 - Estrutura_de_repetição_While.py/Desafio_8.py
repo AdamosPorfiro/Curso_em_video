562e2b8d5d18d088eee2457784d857ae1b3202cd
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-12 16:56:39 -0300

    Added_logic.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_8.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_8.py
@@ -1,6 +1,33 @@
 '''
-Crie um programa que lei varios números inteiros pelo teclado. O programa só
-vvai parar quando o usuario digitar o valor 999, que é a condição de parada.
-No final mostre quantos numeros foram digitados e qual foi a soma entre entre
+Crie um programa que leia varios números inteiros pelo teclado. 
+O programa só vai parar quando o usuario digitar o valor 999, que é a condição de parada.
+No final mostre quantos numeros foram digitados e qual foi a soma entre eles.
 (Desconsiderando o flag)
+
+1 Quais são os dados de entrada?
+input numero
+
+2 O que devo fazer com esses dados?
+Exibir quantos numeros foram digitados;
+Exibir soma entre esses numero;
+Deve desconsiderar o numero o flag dessa analise (Condição de parada).
+
+3 Quais são as restrições desse problema?
+Deve desconsiderar o numero flag da analise;
+Numero devem ser inteiros;
+
+4 Qual o resultado esperado?
+Ler os numeros digitados;
+Exibir quantidade de numeros que foram digitados;
+Exibir a soma desses numeros;
+Desconsiderar o numero 999 sendo que ele é o flag;
+
+5 Quais são os passos necessarios para se alcançar o resultado esperado?
+
+saida = 999
+while saida != 999
+    n1 = input numero
+
+
+
 '''
