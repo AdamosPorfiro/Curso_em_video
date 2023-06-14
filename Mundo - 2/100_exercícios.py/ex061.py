ad8d3a74f34eb9031cbf7dd88bed40d9097e6392
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-14 15:42:13 -0300

    Added_arrow_line_16.py

--- Mundo - 2/100_exercícios.py/ex061.py
+++ Mundo - 2/100_exercícios.py/ex061.py
@@ -13,6 +13,7 @@ contador = 0 # CONTADOR DE 0 A 10;
 print('\n{:^10}Os 10 primeiros termos dessa progressão é:\n'.format('')) # EXIBIR
 while contador < 10: # EM QUANTO O CONTADOR FOR MENOR QUE 10 O BLOCO ABAIXO SERÁ EXECUTADO;
     termo = n1 + contador*2 # CALCULA OS TERMOS PARA EXIBIR A PROGRESSÃO
-    print('➝ {:^4}'.format(termo),end='') # EXIBI OS TERMOS
+    print ('➝ ' if contador < 10 else '',end='')
+    print('{:^4}'.format(termo),end='') # EXIBI OS TERMOS
     contador += 1 # SOMA MAIS UM NO CONTADOR
 print('\n{:=^60}'.format(''))
