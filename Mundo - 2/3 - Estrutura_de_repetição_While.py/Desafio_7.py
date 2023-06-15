a0951f0e994c04881872804f5c7873dea1531a6d
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2023-06-14 23:40:04 -0300

    Change_code.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_7.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_7.py
@@ -1,4 +1,4 @@
-'''
+"""
 Escreva um programa que leia um número n inteiro qualquer e mostre na tela os n
 primeiros elementos de uma SEQUÊNCIA DE FIBONACCI.
 
@@ -14,19 +14,20 @@ T = 15
 Assim, temos a sequência:
 
  8 > 15 > 29 > 57 > 113...n
-'''
-print('{:=^40}\n{:^8}SEQUÊNCIA DE FIBONACCI\n{:=^40}\n'.format('','',''))
-n1 = float(input('{:^9}Informe um número: '.format('')))
-'''for c in range(1,6):
+"""
+print("{:=^44}\n{:^11}SEQUÊNCIA DE FIBONACCI\n{:=^44}\n".format("", "", ""))
+n1 = int(input("{:^6}Quantos termos você quer ver? ".format("")))
+"""for c in range(1,6):
     n1 = n1-1+n1
-    print('> {} '.format(n1), end='')'''
-Repetidor = 0
-while Repetidor < 5:
-    n1 = n1-1+n1
+    print('> {} '.format(n1), end='')"""
+Repetidor = 2
+t1 = 0
+t2 = 1
+print("{} ➝ {} ➝ ".format(t1, t2), end="")
+while Repetidor < n1:
+    t3 = t1 + t2
     Repetidor += 1
-    print('> {} '.format(n1), end='')
-print('\n{:=^40}'.format(''))
-
-
-
-
+    print("{} ➝ ".format(t3), "FIM" if Repetidor == n1 else "", end="")
+    t1 = t2
+    t2 = t3
+print("\n{:=^44}".format(""))
