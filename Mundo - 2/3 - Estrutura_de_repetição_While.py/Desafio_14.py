036823528b8d27d0187fd50c09e646eba8615655
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-20 18:20:12 -0300

    Added_code.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_14.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_14.py
@@ -6,4 +6,22 @@ No final, mostrar:
 A - Qual é o total gasto na compra
 B - Quantos produtos custam mais que R$ 1000
 C - Qual é o nome do produto mais barato
+
 '''
+Total_gasto = 0
+while True:
+    while True:
+        Produto = str(input('Produto: '))
+        Preço = str(input('Preço R$ '))
+        while True:
+            Sair_continuar = str(input('Continuar [S/N]: ')).upper().strip()[0]
+            if Sair_continuar not in 'SN':
+                continue
+            elif Sair_continuar in 'N':
+                break
+            elif Sair_continuar in 'S':
+                break
+        if Sair_continuar in 'S':
+            print(f'\nQual é o total gasto R$ {}\nProdutos custam mais de R$ 1000: {}\nNome do produto mais caro: {}')
+            break
+    break
