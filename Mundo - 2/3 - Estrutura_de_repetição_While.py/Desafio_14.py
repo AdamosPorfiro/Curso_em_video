7c7cdb1b66560e2ebf4182a9d60f11528e832581
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2023-06-20 23:21:47 -0300

    Added_condition.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_14.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_14.py
@@ -8,20 +8,28 @@ B - Quantos produtos custam mais que R$ 1000
 C - Qual é o nome do produto mais barato
 
 '''
-Total_gasto = 0
+Total_gasto  = Preco_produto_barato = 0
+Lista_produtos = []
+Produto_mais_barato  = ''
 while True:
     while True:
         Produto = str(input('Produto: '))
-        Preço = str(input('Preço R$ '))
+        Preco = float(input('Preço R$ '))
+        Total_gasto+=Preco
+        if Preco >= 1000:
+            Lista_produtos.append(Produto)
+        if Preco_produto_barato < Preco:
+            Preco_produto_barato = Preco
+            Produto_mais_barato = Produto
         while True:
             Sair_continuar = str(input('Continuar [S/N]: ')).upper().strip()[0]
             if Sair_continuar not in 'SN':
                 continue
-            elif Sair_continuar in 'N':
-                break
             elif Sair_continuar in 'S':
                 break
-        if Sair_continuar in 'S':
-            print(f'\nQual é o total gasto R$ {}\nProdutos custam mais de R$ 1000: {}\nNome do produto mais caro: {}')
+            elif Sair_continuar in 'N':
+                break
+        if Sair_continuar in 'N':
             break
     break
+print(f'\nTotal gasto R$ {Total_gasto:,}\nProdutos de R$ 1000: {Lista_produtos}\nProduto mais barato: {Produto_mais_barato}' )
