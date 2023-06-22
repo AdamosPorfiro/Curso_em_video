6384c1c29b2830a389bfc33ec6ca82ba7d851d09
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2023-06-22 09:22:05 -0300

    added_innitial_code.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_15.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_15.py
@@ -5,4 +5,26 @@ e o programa vai dizer quantas cedulas de cada valor serão entregues.
 
 OBS: Considere que  o caixa possui cedulas de:
 R$50 R$20 R$10 e R$1
+
+Vamos desenvolver um loop infinito que irá repetir todo o bloco;
 '''
+Cedulas_50 = 50
+Cedulas_20 = 20
+Cedulas_10 = 10
+Cedulas_1 = 1
+while True:
+    n = str(input('Qual valor a ser sacado R$ '))
+    if n.isdigit():
+        break
+    else:
+        continue # Funciona
+'''
+Dizer para o usuario quantas cedulas de cada valor serão entregues;
+'''
+n=int(n)
+for c in range(1,50):
+    
+
+
+
+
