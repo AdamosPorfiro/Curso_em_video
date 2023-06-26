702fd936b733bda4b6ff1c5d29b477db71e38930
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-26 17:27:19 -0300

    Added_new_code.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_15.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_15.py
@@ -5,25 +5,29 @@ e o programa vai dizer quantas cedulas de cada valor serão entregues.
 
 OBS: Considere que  o caixa possui cedulas de:
 R$50 R$20 R$10 e R$1
-
-Vamos desenvolver um loop infinito que irá repetir todo o bloco;
 '''
 print('\n{:=^30}\n{:>7}BANCO DO ADAMOS\n{:=^30}'.format('','',''))
 saque = int(input('Qual o valor a ser sacado R$ '))
+total = saque
 total_ced = 0
 ced = 50
 while True:
-    if saque >= 50:
-        saque -= ced
+    if total >= ced:
+        total -= ced
         total_ced+=1 
-        print(f'Cedulas R$ 50: {total_ced}')
-        if total_ced < 50:
+    else:
+        if total_ced > 0:
+            print(f'Total {total_ced} cédulas de R$ {ced}') 
+        if ced == 50:
             ced = 20
-            total_ced+=1
-             
-    if saque < 50:
-        break
-print(f'Quantidade de cédulas de R$ 50: {total_ced} cédulas') 
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        total_ced = 0
+        if total == 0:
+            break
+
 
 
 
