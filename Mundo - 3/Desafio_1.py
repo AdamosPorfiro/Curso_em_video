7d37ac374328aa5e3a1d8871ebc201f26965da97
Adamos Porfiro <adamos.contato@gmail.com>
2023-06-30 18:00:45 -0300

    Added_start_code.py

--- Mundo - 3/Desafio_1.py
+++ Mundo - 3/Desafio_1.py
@@ -1,4 +1,17 @@
 '''
 Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero
 até vinte. Seu programa deverá ler um numero pelo teclado(entre 0 e 20) e mostra-lo por extenso.
+
+Dados de entrada?
+1 tupla totalmente preenchida com um contagem escrita por extenso, do numero zero ao vinte!
+
+
 '''
+n_extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 
+             'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 
+             'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze',
+             'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito',
+             'Dezenove', 'Vinte')
+n = int(input('Digite um número entre 0 e 20: '))
+for cont in n_extenso:
+    
