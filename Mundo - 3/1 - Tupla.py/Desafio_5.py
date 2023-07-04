fc3040c373127ae5ad281322f6a29c9efe5610c6
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-04 17:38:59 -0300

    added_changelle_5.py

--- Mundo - 3/1 - Tupla.py/Desafio_5.py
+++ Mundo - 3/1 - Tupla.py/Desafio_5.py
@@ -5,13 +5,13 @@ na sequência.
 No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 '''
 from tabulate import tabulate
-Produtos = [('Caneta', 2.00),
-            ('Lapis', 2.50),
-            ('Sabonete liquido', 12.50),
-            ('Chinelo', 18.00),
-            ('Porta retrato', 16.50)]
+Produtos = [('Caneta............', 2.00),
+            ('Lapis.............', 2.50),
+            ('Sabonete liquido..', 12.50),
+            ('Chinelo...........', 18.00),
+            ('Porta retrato.....', 16.50)]
 
-colunas = ['Produtos', 'Valores R$']
-tabela = tabulate(Produtos, headers=colunas)
+colunas = ['Produtos', 'R$']
+tabela = tabulate(Produtos, headers=colunas, floatfmt=".2f")
 print(tabela)
 
