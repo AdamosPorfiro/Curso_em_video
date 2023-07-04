3668caacb577649db598287144df3a79f00ba132
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-04 16:51:36 -0300

    added_code

--- Mundo - 3/1 - Tupla.py/Desafio_5.py
+++ Mundo - 3/1 - Tupla.py/Desafio_5.py
@@ -4,3 +4,14 @@ na sequência.
 
 No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 '''
+from tabulate import tabulate
+Produtos = [('Caneta', 2.00),
+            ('Lapis', 2.50),
+            ('Sabonete liquido', 12.50),
+            ('Chinelo', 18.00),
+            ('Porta retrato', 16.50)]
+
+colunas = ['Produtos', 'Valores R$']
+tabela = tabulate(Produtos, headers=colunas)
+print(tabela)
+
