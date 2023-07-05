fa0c4ecf91c81e71dc616b61969789076691169f
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-05 16:29:39 -0300

    Added_changelle_s6_and_ex075.py

--- Mundo - 3/100_exercícios.py/ex075.py
+++ Mundo - 3/100_exercícios.py/ex075.py
@@ -2,13 +2,10 @@
 Crie um programa que tenha várias palavras(Não usar acentos).
 Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 '''
-
-Palavras = ('Sabonete', 'Aroma', 'Chinelo', 'Porta retrato', 'Presente', 'Placa', 'Sacola')
-for p in Palavras:
+palavras = ('Sabonete', 'Sacola', 'Estojo', 'Porta retrato', 'Chinelo')
+for p in palavras:
     vogais = ''
     for l in p:
         if l.lower() in 'aeiou':
             vogais+=l
-    print(f'A palavra \033[32m{p}\033[m tem as vogais: \33[41m{vogais}\33[m')
-   
-       
+    print(f'A palavra {p} tem as vogais {vogais}')
