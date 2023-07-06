c11090e68791362954587083f9df61718dad50fa
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-06 17:18:34 -0300

    Added_ex076.py

---
+++ Mundo - 3/100_exercícios.py/ex076.py
@@ -0,0 +1,11 @@
+'''
+Crie um programa que tenha várias palavras(Não usar acentos).
+Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+'''
+Palavras = ('Cantar', 'Sabonete', 'Montanha', 'Arvores')
+for p in Palavras:
+    vogais = ''
+    for v in p:
+        if v.lower() in 'aeiou':
+            vogais+=v
+    print(f'A palavra {p} tem as vogais \033[41m{vogais}\033[m')
