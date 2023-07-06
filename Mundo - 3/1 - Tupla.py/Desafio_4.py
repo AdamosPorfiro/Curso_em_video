67bf8bd04b519fe5c2df87c87d0de6294120e615
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-06 14:36:23 -0300

    Added_new_code.py

--- Mundo - 3/1 - Tupla.py/Desafio_4.py
+++ Mundo - 3/1 - Tupla.py/Desafio_4.py
@@ -4,7 +4,7 @@ Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma t
 A) Quantas vezes apareceu o valor 9
 B) Em que posição foi digitado o primeiro valor 3.
 C) Quais foram os numeros pares.
-'''
+
 Lista = []
 Pares = []
 qtd_9  = indice_3 = 0
@@ -20,5 +20,20 @@ for indice,valor in enumerate(convert_tupla):
         indice_3 = indice + 1 
     if valor % 2 == 0:
         Pares.append(valor)
-print(f'''Você digitou os valores: {convert_tupla}\nO valor 9 apareceu: {qtd_9} vezes\nA posição que o primeiro 3 foi digitado foi: {indice_3}
-Os numeros pares são: {Pares}''')
+print(f''Você digitou os valores: {convert_tupla}\nO valor 9 apareceu: {qtd_9} vezes\nA posição que o primeiro 3 foi digitado foi: {indice_3}
+Os numeros pares são: {Pares}'')
+'''
+n = (int(input('Digite um numero: ')),
+    int(input('Digite outro um numero: ')), 
+    int(input('Digite mais um numero: ')),
+    int(input('Digite um último numero: ')))
+print(f'\nVocê digitou os valores: {n}')
+print(f'O número nove foi digitado {n.count(9)} vezes.')
+if 3 in n:
+    print (f'O primeiro número três está na posição {n.index(3)+1}º.')
+else:
+    print('O número 3 não foi digitado!')
+print(f'Os números pares digitados são: ', end='')
+for c in n:
+    if c % 2 == 0:
+        print(f'{c} ', end='')
--- Mundo - 3/100_exercícios.py/ex074.py
+++ Mundo - 3/100_exercícios.py/ex074.py
@@ -4,7 +4,7 @@ Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma t
 A) Quantas vezes apareceu o valor 9
 B) Em que posição foi digitado o primeiro valor 3.
 C) Quais foram os numeros pares.
-'''
+
 Lista = []
 Pares = []
 qtd_9  = indice_3 = 0
@@ -20,5 +20,20 @@ for indice,valor in enumerate(convert_tupla):
         indice_3 = indice + 1 
     if valor % 2 == 0:
         Pares.append(valor)
-print(f'''Você digitou os valores: {convert_tupla}\nO valor 9 apareceu: {qtd_9} vezes\nA posição que o primeiro 3 foi digitado foi: {indice_3}
-Os numeros pares são: {Pares}''')
+print(f''Você digitou os valores: {convert_tupla}\nO valor 9 apareceu: {qtd_9} vezes\nA posição que o primeiro 3 foi digitado foi: {indice_3}
+Os numeros pares são: {Pares}'')
+'''
+n = (int(input('Digite um número: ')),
+     int(input('Digite outro número: ')),         
+     int(input('Digite mais um número: ')),
+     int(input('Digite um útilmo número: ')))
+print(f'\nOs valores digitados foram: {n}')
+print(f'O número 9 foi digitado {n.count(9)} vezes')
+if 3 in n:
+    print(f'O número 3 está na {n.index(3)+1}º posição')
+else:
+    print('O número 3 não foi digitado!')
+print('Os números pares digitados foram: ', end='')
+for c in n:
+    if c % 2 == 0:
+        print(f'{c} ', end='')
