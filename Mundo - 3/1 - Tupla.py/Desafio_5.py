387da9d7461004245e35fde8953c4e91328f7d45
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-06 17:10:24 -0300

    change_full_code.py

--- Mundo - 3/1 - Tupla.py/Desafio_5.py
+++ Mundo - 3/1 - Tupla.py/Desafio_5.py
@@ -3,17 +3,28 @@ Crie um programa que tenha uma tupla única com nomes de produtos e seus respect
 na sequência.
 
 No final, mostre uma listagem de preços, organizando os dados em forma tabular.
-'''
-from tabulate import tabulate
-Produtos = (('Caneta............', 2.00),
-            ('Lapis.............', 2.50),
-            ('Sabonete liquido..', 12.50),
-            ('Chinelo...........', 18.00),
-            ('Porta retrato.....', 16.50)) # Tupla aninhadas
 
+from tabulate import tabulate
+Produtos = (('Caneta..........R$', 2.00),
+            ('Lapis...........R$', 2.50),
+            ('Sabonete........R$', 12.50),
+            ('Chinelo.........R$', 18.00),
+            ('Porta retrato...R$', 16.50)) # Tupla aninhadas
+print('{:-^25}\n{:>3}Listagem de produtos\n{:-^25}'.format('','',''))
 colunas = ['Produtos', 'R$']
 tabela = tabulate(Produtos, headers=colunas, floatfmt=".2f") # Formatação da tabela
-print(tabela)
+print(tabela,'\n')
+'''
+
+Lista = ('Lapis', 2.00,
+         'Caneta', 2.00,
+         'Sabonete', 12.50,
+         'Chinelo', 25.00,)
+for n in range(0, len(Lista)):
+    if n % 2 == 0:
+        print(f'{Lista[n]:.<35}', end='')
+    else:
+        print(f'R$ {Lista[n]:>5.2f}')
 
 
 
--- Mundo - 3/100_exercícios.py/ex075.py
+++ Mundo - 3/100_exercícios.py/ex075.py
@@ -1,11 +1,15 @@
 '''
-Crie um programa que tenha várias palavras(Não usar acentos).
-Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
+Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços
+na sequência.
+
+No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 '''
-palavras = ('Sabonete', 'Sacola', 'Estojo', 'Porta retrato', 'Chinelo')
-for p in palavras:
-    vogais = ''
-    for l in p:
-        if l.lower() in 'aeiou':
-            vogais+=l
-    print(f'A palavra {p} tem as vogais {vogais}')
+Lista = ('Oculos', 36.00,
+         'Caneta', 2.00,
+         'Tinta guache', 10.00,
+         'Sabonete liquido', 12.50)
+for n in range(0,len(Lista)):
+    if n % 2 == 0:
+        print(f'{Lista[n]:.<30}',end='')
+    else:
+        print(f'R$ {Lista[n]:>5.2f}')
