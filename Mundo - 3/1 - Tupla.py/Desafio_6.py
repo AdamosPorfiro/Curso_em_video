721518264a9411682771c694d15052127627444b
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2023-07-06 21:24:11 -0300

    Added_new_code.py

--- Mundo - 3/1 - Tupla.py/Desafio_6.py
+++ Mundo - 3/1 - Tupla.py/Desafio_6.py
@@ -1,7 +1,6 @@
 '''
 Crie um programa que tenha várias palavras(Não usar acentos).
 Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
-'''
 
 Palavras = ('Sabonete', 'Aroma', 'Chinelo', 'Porta retrato', 'Presente', 'Placa', 'Sacola')
 for p in Palavras:
@@ -10,5 +9,12 @@ for p in Palavras:
         if l.lower() in 'aeiou':
             vogais+=l
     print(f'A palavra \033[32m{p}\033[m tem as vogais: \33[41m{vogais}\33[m')
-   
+'''
+Palavras = ('Sabonete', 'Aroma', 'Chinelo', 'Porta retrato', 'Presente', 'Placa', 'Sacola')
+for p in Palavras:
+    print(f'\nNa palavra {p.upper()} temos as vogais: ', end='')
+    for l in p:
+        if l.lower() in 'aeiou':
+            print(f'{l}',end='')
+
        
--- Mundo - 3/100_exercícios.py/ex076.py
+++ Mundo - 3/100_exercícios.py/ex076.py
@@ -1,7 +1,7 @@
 '''
 Crie um programa que tenha várias palavras(Não usar acentos).
 Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
-'''
+
 Palavras = ('Cantar', 'Sabonete', 'Montanha', 'Arvores')
 for p in Palavras:
     vogais = ''
@@ -9,3 +9,10 @@ for p in Palavras:
         if v.lower() in 'aeiou':
             vogais+=v
     print(f'A palavra {p} tem as vogais \033[41m{vogais}\033[m')
+    '''
+Palavras = ('Cantar', 'Sabonete', 'Montanha', 'Arvores')
+for p in Palavras:
+    print(f'\nNa palavra {p.upper()} temos: ',end='')
+    for l in p:
+        if l.lower() in 'aeio':
+            print(f'{l}',end='')
