90bb3c76f8d28c3ef8e0887d1eb4e0bfa37f41b0
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-08 17:33:33 -0300

    added_code.py

--- Mundo - 3/1 - Lista.py/Desafio_02.py
+++ Mundo - 3/1 - Lista.py/Desafio_02.py
@@ -4,3 +4,17 @@ uma lista. Caso o número já exista lá dentro, ele não será adicionado.
 
 No finnal, serão exibidos todos os valores únicos digitados, em ordem crescente
 '''
+l_n = []
+while True:
+    n = int(input('Digite um número: '))
+    l_n.append(n)
+    for i,c in enumerate(l_n):
+        if c == n:
+            
+    continuar = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+    if continuar in 'S':
+        continue
+    else:
+        break
+l_n.sort()
+print(f'Você digitou os valores {l_n}')
