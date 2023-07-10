f38ce93e5f5ed0ffb8b9c2e8bc411c864c43cddc
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-10 15:02:11 -0300

    Change_line_5.py

--- Mundo - 3/1 - Lista.py/Desafio_06.py
+++ Mundo - 3/1 - Lista.py/Desafio_06.py
@@ -2,5 +2,4 @@
 Crie um programa onde o usuario digite uma expressão qualquer que usa parentêses, seu
 aplicativo deverá analisar se a expressão passada está com os parênteses abertos e 
 fechado na ordem correta.
-
 '''
