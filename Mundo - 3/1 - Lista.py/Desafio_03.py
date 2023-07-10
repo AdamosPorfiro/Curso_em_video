4e913e3836ef38176c30e31c0702e9c14f632c93
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2023-07-09 22:28:02 -0300

    Added_soluction.py

--- Mundo - 3/1 - Lista.py/Desafio_03.py
+++ Mundo - 3/1 - Lista.py/Desafio_03.py
@@ -1,6 +1,17 @@
-'''
+"""
 Crie um programa onde o usuario pode digitar cinco valores númericos e cadastre-os em uma lista,
-já na posição correta de inserção (sem usar o srt())
+já na posição correta de inserção (sem usar o sort())
 
 No final, mostre a lista ordenada na tela.
-'''
+"""
+l_n = []
+for c in range(5):
+    n = int(input("Digite um número: "))
+    if len(l_n) == 0:
+        l_n.append(n)
+    else:
+        index = 0
+        while index < len(l_n) and n > l_n[index]:
+            index += 1
+            l_n.insert(index, n)
+print(f"Você digitou os valores {l_n}")
