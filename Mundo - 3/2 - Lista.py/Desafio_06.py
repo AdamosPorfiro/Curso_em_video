7f4508066bc841bdbe29f7b359beda49bcd3fb08
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-10 17:13:00 -0300

    import_function.py

--- Mundo - 3/2 - Lista.py/Desafio_06.py
+++ Mundo - 3/2 - Lista.py/Desafio_06.py
@@ -3,3 +3,4 @@ Crie um programa onde o usuario digite uma expressão qualquer que usa parentês
 aplicativo deverá analisar se a expressão passada está com os parênteses abertos e 
 fechado na ordem correta.
 '''
+from sympy import sympify, symbols
