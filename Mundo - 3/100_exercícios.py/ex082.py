867eb775365004c1fb468168634c011c6667f7ba
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-14 15:02:27 -0300

    Added_soluction.py

--- Mundo - 3/100_exercícios.py/ex082.py
+++ Mundo - 3/100_exercícios.py/ex082.py
@@ -2,13 +2,29 @@
 Crie um programa onde o usuario digite uma expressão qualquer que usa parentêses, seu
 aplicativo deverá analisar se a expressão passada está com os parênteses abertos e 
 fechado na ordem correta.
-'''
-from sympy import sympify, symbols
-expr = input('Digite uma expressão matematica: ')
-Simbolos = symbols('x y ()')
+
+from sympy import sympify, symbols # Modulo sympy que faz verificação de expressões matematicas
+expr = input('Digite uma expressão: ') # Input expressão
+Simbolos = symbols('x y ()') #
 try:
     expr = sympify(expr)
-    print('Expressão é válida!')
+    print('A expressão é valida')
 
-except(SyntaxError,TypeError):
-    print('Expressão não é válida!')
+except(SyntaxError, TypeError):
+    print('Expressão Invalida!')
+'''
+expr = str(input('Digite uma expressão: '))
+pilha = []
+for símb in expr:
+    if símb == '(':
+        pilha.append('(')
+    elif símb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está invalida!')
--- Mundo - 3/2 - Lista.py/Desafio_06.py
+++ Mundo - 3/2 - Lista.py/Desafio_06.py
@@ -2,7 +2,7 @@
 Crie um programa onde o usuario digite uma expressão qualquer que usa parentêses, seu
 aplicativo deverá analisar se a expressão passada está com os parênteses abertos e 
 fechado na ordem correta.
-'''
+
 from sympy import sympify, symbols # Modulo sympy que faz verificação de expressões matematicas
 expr = input('Digite uma expressão: ') # Input expressão
 Simbolos = symbols('x y ()') #
@@ -12,3 +12,19 @@ try:
 
 except(SyntaxError, TypeError):
     print('Expressão Invalida!')
+'''
+expr = str(input('Digite uma expressão: '))
+pilha = []
+for símb in expr:
+    if símb == '(':
+        pilha.append('(')
+    elif símb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está válida!')
+else:
+    print('Sua expressão está invalida!')
