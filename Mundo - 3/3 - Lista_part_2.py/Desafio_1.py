385dd9cf941db2306fb32d891f1bb729577a280e
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-17 18:23:22 -0300

    Added_code.py

--- Mundo - 3/3 - Lista_part_2.py/Desafio_1.py
+++ Mundo - 3/3 - Lista_part_2.py/Desafio_1.py
@@ -7,3 +7,18 @@ b - Uma listagem com as pessoas mais pesadas
 
 c - Uma listagem com as pessoas mais leve
 '''
+Armazenador = []
+Nome_Peso = []
+Cadastros = 0
+while True:
+    Armazenador.append(str(input('Digite o seu nome: ')))
+    Armazenador.append(float(input('Digite o seu peso: ')))
+    Nome_Peso.append(Armazenador[:])
+    Armazenador.clear()
+    Cadastros += 1
+    resp = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+    if resp == 'N':
+        break
+
+# Quantas pessoas foram cadastradas?
+print(f'Quantidade de pessoas cadastradas: {Cadastros}')
