e7ed05980753901ec33c686fe27b360e3c4ac0ff
Adamos Porfiro <adamos.contato@gmail.com>
2023-07-18 17:40:01 -0300

    Added_code.py

--- Mundo - 3/3 - Lista_part_2.py/Desafio_1.py
+++ Mundo - 3/3 - Lista_part_2.py/Desafio_1.py
@@ -9,7 +9,9 @@ c - Uma listagem com as pessoas mais leve
 '''
 Armazenador = []
 Nome_Peso = []
-Cadastros = 0
+Cadastros  = 0
+pesos = []
+n_maior_p = ''
 while True:
     Armazenador.append(str(input('Digite o seu nome: ')))
     Armazenador.append(float(input('Digite o seu peso: ')))
@@ -19,6 +21,15 @@ while True:
     resp = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
     if resp == 'N':
         break
+while True:
+    for c in Nome_Peso:
+        if c[1] > pesos[:-1]:
+            pesos.append(c[1])
+    for p in pesos:
+            if c[1] > p:
+                pesos.append(c[1])
 
 # Quantas pessoas foram cadastradas?
-print(f'Quantidade de pessoas cadastradas: {Cadastros}')
+print(f'Quantidade de pessoas cadastradas: {Cadastros}.')
+# Listagem pessoas mais pesadas
+print(f'O maior é peso é do(a) {n_maior_p} com {peso} kg.')
