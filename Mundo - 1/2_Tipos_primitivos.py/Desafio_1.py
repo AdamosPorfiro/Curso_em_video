82a287ac74718b8a3204eff1a7d0efeb2edcfcd9
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-08 22:18:34 -0300

    Rename.py

--- Mundo - 1/2_Tipos_primitivos.py/Desafio_1.py
+++
@@ -1,9 +0,0 @@
-#Crie um programa que leia dois numeros e mostre a soma deles
-
-print ('===== Desafio 1 =====')
-n1 = (input('Digite um numero: '))
-n2 = (input('Digite mais um numero  para soma-los: '))
-"""print("Valor digitado é numerico? ", n1.isnumeric())
-print("Valor digitado é numerico?",n2.isnumeric())"""
-resultado = int(n1) + int(n2)
-print ('A soma de {} + {} = {}' .format (n1, n2, resultado))
---
+++ Mundo - 1/2_Tipos_primitivos.py/Desafio_4.py
@@ -0,0 +1,13 @@
+#Crie um programa que leia dois numeros e mostre a soma deles
+
+print ('===== Desafio 4 =====')
+n1 = (input('Digite algo: '))
+print("\nTrue = Sim / False = Não")
+print("O valor digitado e numerico? ", n1.isnumeric())
+print("O valor digitado e uma string? ", n1.isalpha())
+print("O valor digitado e um numero? ", n1.isalnum())
+print("O valor digitado esta em maiusculo? ", n1.isupper())
+print("O valor digitado esta em minusculo? ", n1.islower())
+print("O valor digitado é imprimivel ? ", n1.isprintable())
+"""resultado = int(n1) + int(n2)
+print ('A soma de {} + {} = {}' .format (n1, n2, resultado))"""
