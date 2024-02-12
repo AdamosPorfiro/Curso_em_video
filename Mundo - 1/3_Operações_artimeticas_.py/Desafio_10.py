4ef525a09ec32ebdd7ea198f9ee4ef4c98d5d781
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-12 16:31:02 -0300

    Desafio_10.py

--- Mundo - 1/3_Operações_artimeticas_.py/Desafio_10.py
+++ Mundo - 1/3_Operações_artimeticas_.py/Desafio_10.py
@@ -12,5 +12,5 @@ print("{:=^45}".format(" Desafio 10 "))
 print("{:^45}".format("Cotação: U$$ 3,27"))
 n = float(input("  Quanto possui de dinheiro em REAIS R$: "))
 qtd_dolar = n/3.27
-print("\nCom R$ {}\nVocê pode adiquir U$$ {:.1f}".format(n, qtd_dolar))
+print("\nCom R$ {}\nVocê pode adiquir U$$ {:.2f}".format(n, qtd_dolar))
 print("{:=^45}".format(""))
