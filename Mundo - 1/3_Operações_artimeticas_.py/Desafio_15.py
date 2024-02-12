2e58842dfddd35db215c3500497608c4e89e594f
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-12 17:16:48 -0300

    Desafio_15.py

--- Mundo - 1/3_Operações_artimeticas_.py/Desafio_15.py
+++ Mundo - 1/3_Operações_artimeticas_.py/Desafio_15.py
@@ -6,8 +6,10 @@ Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km
 
 """
 
+print("{:=^40}".format(" Desafio 15 "))
 dias = int(input("Por quantos dias o carro foi alugado?  "))
 qtd_km = float(input("Quantos km foram rodados? "))
-vl_dia = dias*60
-vl_km = qtd_km*0.15
-print("\n\nVocê alugou por {} dias.\nRodou {}km.\nO Valor total dos dias R$ {:.2f}\nO Valor total em km R$ {:.2f}\nO Total a pagar R$ {}".format(dias,qtd_km,vl_dia,vl_km,vl_dia+vl_km))
+Total = dias*60+qtd_km*0.15
+
+print("\nTotal a pagar R$ {}".format(Total))
+print("{:=^40}".format(""))
