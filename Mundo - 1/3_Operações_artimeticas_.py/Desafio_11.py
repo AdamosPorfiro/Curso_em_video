7c601bd083383e8a4e1fca21cc164dfbf4206bc5
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-12 16:32:56 -0300

    Desafio_11.py

--- Mundo - 1/3_Operações_artimeticas_.py/Desafio_11.py
+++ Mundo - 1/3_Operações_artimeticas_.py/Desafio_11.py
@@ -10,5 +10,5 @@ l = float(input("Informe a largura da parede: "))
 a = float(input("Informe a altura da parede: "))
 area = l*a
 qtd_tinta = area/2
-print("\nArea total: {} m²\nQuantidade de tinta: {} litros".format(area,round(qtd_tinta)))
+print("\nArea total: {} m²\nQuantidade de tinta: {} litros".format(area,qtd_tinta))
 print("{:=^40}".format(""))
