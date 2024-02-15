44e1fc3c70e51d966990f698fb562c711d5514cf
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-15 15:19:18 -0300

    Desafio_18.py

--- Mundo - 1/4 - Modulos_em_python.py/Desafio_18.py
+++ Mundo - 1/4 - Modulos_em_python.py/Desafio_18.py
@@ -3,6 +3,7 @@ Faça um programa que leia um angulo qualquer e mostre na tela o valor do seno, 
 
 """
 from math import sin,cos,tan, radians
+from time import sleep
 
 print("{:=^40}".format(" Desafio 18 "))
 ang = float(input("Digite um angulo: "))
@@ -10,6 +11,7 @@ ang = float(input("Digite um angulo: "))
 seno = sin(radians(ang))
 coss = cos(radians(ang))
 tang = tan(radians(ang))
-
-print("\nSeno {}\nCosseno {}\nTangente {}".format(seno, coss, tang))
+print("Calculando Angulo SENO, COSSENO e TANGENTE...")
+sleep(2.5)
+print("\nSeno {:.2f}\nCosseno {:.2f}\nTangente {:.2f}".format(seno, coss, tang))
 print("{:=^40}".format(""))
