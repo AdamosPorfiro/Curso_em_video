7213f8d5bb7f3bc0e326ce8a65bf89945ad2fa0d
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-14 21:47:32 -0300

    Desafio_17.py

--- Mundo - 1/4 - Modulos_em_python.py/Desafio_17.py
+++ Mundo - 1/4 - Modulos_em_python.py/Desafio_17.py
@@ -5,7 +5,9 @@ Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente 
 Calcule e mostre o comprimento da hipotenusa
 
 """
+print("{:=^50}".format(" Desafio 17 "))
 from math import hypot
 cat_opo = float(input("Informe o cateto oposto: "))
 cat_adj = float(input("Informe o cateto adjacente: "))
-print("\nO cateto oposto é {}.\nO cateto adjacente é {}.\nA hipotenusa é {:.2f}.".format(cat_opo, cat_adj, hypot(cat_opo, cat_adj))) 
+print("\nO cateto oposto é {}\nO cateto adjacente é {}\nA hipotenusa é {:.2f}".format(cat_opo, cat_adj, hypot(cat_opo, cat_adj))) 
+print("{:=^50}".format(""))
