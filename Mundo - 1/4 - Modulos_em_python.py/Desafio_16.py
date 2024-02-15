718f587d2b6498a79b1b8c73f709379e35fc6c8f
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-15 14:53:45 -0300

    Desafio_16.py

--- Mundo - 1/4 - Modulos_em_python.py/Desafio_16.py
+++ Mundo - 1/4 - Modulos_em_python.py/Desafio_16.py
@@ -6,8 +6,13 @@ Crie um programa que leia um numero real qualquer pelo teclado e mostre na tela 
 Ex: Digite um numero: 6.127 o numero 6.127 tem a parte inteira 6.
 
 """
+"""
 print("{:=^50}".format(" Desafio 16 "))
 from math import trunc
 num = float(input("Digite um numero real: ")) 
 print("O valor digitado foi: {} e sua porção inteira é: {}".format(num, trunc(num))) # Vai truncar o numero e mostrar apenas o numero inteiro
 print("{:=^50}".format(""))
+"""
+#Opção sem uso de modulos
+num = float(input("Digite um numero: "))
+print("\nO valro digitado foi {} e sua porção inteira é {}".format(num,int(num)))
