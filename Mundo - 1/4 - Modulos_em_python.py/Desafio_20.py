651a7b334f19df6771e1ddafa8b3bec496901b8b
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-15 14:50:07 -0300

    Desafio_20.py

--- Mundo - 1/4 - Modulos_em_python.py/Desafio_20.py
+++ Mundo - 1/4 - Modulos_em_python.py/Desafio_20.py
@@ -3,7 +3,7 @@ Desafio 20
 O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
 Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
 """
-
+print("{:=^50}".format(" Desafio 16 "))
 import random
 
 nome_1 = input("Nome do 1° aluno: ")
@@ -13,4 +13,5 @@ nome_4 = input("Nome do 4° aluno: ")
 
 ord_sort = random.sample([nome_1,nome_2,nome_3,nome_4], k=4)
 
-print("A orden de apresentção é {}".format(ord_sort))
+print("\nA orden de apresentção é\n{}".format(ord_sort))
+print("{:=^50}".format(""))
