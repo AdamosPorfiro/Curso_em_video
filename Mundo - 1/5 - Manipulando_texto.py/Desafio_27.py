381dffc33d5ee2881fb504ac7b73033f3ec8897e
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-15 21:04:45 -0300

    Desafio_27.py

---
+++ Mundo - 1/5 - Manipulando_texto.py/Desafio_27.py
@@ -0,0 +1,10 @@
+"""
+Desafio 27
+
+Fala um programa que leuia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente.
+
+Ex: Ana Maria de Souza
+primeiro = Ana
+ultimo = Souza
+
+"""
