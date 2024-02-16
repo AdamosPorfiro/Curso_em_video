eca14199bc434bb7f2f37337ab9f4c2862ad97ad
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-15 21:03:03 -0300

    Desafio_26.py

---
+++ Mundo - 1/5 - Manipulando_texto.py/Desafio_26.py
@@ -0,0 +1,9 @@
+"""
+Desafio 26
+
+Faça um programa que leia uma frase pelo teclado e mostre:
+
+- Quantas vezes apareçe a letra "A"
+- Em que posição ela aparece a primeira vez
+- Em que posição ela apareçe a ultima vez
+"""
