1d2eddc9b2d929e988b2e86b83d604fe99782cbe
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-16 22:01:13 -0300

    Desafio_25.py

--- Mundo - 1/5 - Manipulando_texto.py/Desafio_25.py
+++ Mundo - 1/5 - Manipulando_texto.py/Desafio_25.py
@@ -3,3 +3,7 @@ Desafio 25
 
 Crie um programa que leia o nome de uma pessoa e diga se ela tem "SILVA" no nome
 """
+nome = str(input("Nome completo: ")).upper()
+nome = nome.count("SILVA")
+dicionario = {1: "Sim", 0: "Não"}
+print("Possui SILVA no nome? {}".format(dicionario[nome]))
