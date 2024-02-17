3a60b4fa668d4f018c8b1c44b34d4ced55494331
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-17 12:19:55 -0300

    Desafio_22.py

--- Mundo - 1/5 - Manipulando_texto.py/Desafio_22.py
+++ Mundo - 1/5 - Manipulando_texto.py/Desafio_22.py
@@ -9,11 +9,13 @@ Crie um programa que leia o nome completo de uma pessoa e mostre:
 - Quantas letras tem o primeiro nome
 
 """
-
+from time import sleep
 print("{:=^40}".format(" Desafio 22 "))
 nome = str(input("Nome completo: "))
-print(nome.upper()) # Todas maiúsculas
-print(nome.lower()) # Todas minúsculas
+print("\nAnalisando o seu nome...")
+sleep(2.5)
+print("O seu nome com todas as letras maiúsculas {}".format(nome.upper())) # Todas maiúsculas
+print("O seu nome com todas as letras minúsculas".format(nome.lower())) # Todas minúsculas
 print("O seu nome possui {} letras".format(len(''.join(nome.split()))))
 print("O seu primeiro nome tem {} letras".format(len(nome.split()[0])))
 print("{:=^40}".format(""))
