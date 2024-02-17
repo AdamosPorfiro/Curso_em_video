c5e71d1f99c7424253f01ad84d6c38d08e32a484
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-17 11:31:24 -0300

    Desafio_24.py

--- Mundo - 1/5 - Manipulando_texto.py/Desafio_24.py
+++ Mundo - 1/5 - Manipulando_texto.py/Desafio_24.py
@@ -7,7 +7,7 @@ Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com
 print("{:=^30}".format(" Desafio 24 "))
 Name_city = str(input("Digite o nome da cidade: ")).upper() # Receber do usuario
 Separar = Name_city.split() # Separo a string
-Começa = Separar[0] in "SANTO" # Valido os dados se cidade começa ou não com não
+Começa = Separar[0] in "SANTO" # Valido os dados se cidade começa ou não com SANTO
 Dicionario = {True: "Sim", False: "Não"} # Uso mapeamento de dicionario para mapear a validação
 print("A cidade começa com SANTO? {}".format(Dicionario[Começa])) # Exibo o resultado para o usuario
 print("{:=^30}".format(""))
