6793f03e1043245170adf3929e5c8a0cc9022a7e
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-16 23:28:26 -0300

    Desafio_27.py

--- Mundo - 1/5 - Manipulando_texto.py/Desafio_27.py
+++ Mundo - 1/5 - Manipulando_texto.py/Desafio_27.py
@@ -1,10 +1,15 @@
 """
 Desafio 27
 
-Fala um programa que leuia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente.
+Faça um programa que leuia o nome completo de uma pessoa, mostrando em seguida o primeiro e o ultimo nome separadamente.
 
 Ex: Ana Maria de Souza
 primeiro = Ana
 ultimo = Souza
 
 """
+print("{:=^50}".format(" Desafio 27 "))
+nome = str(input("Nome completo: ")).strip().split()
+print("Primeiro nome é: {}".format(nome[0]))
+print("Primeiro nome é: {}".format(nome[-1]))
+print("{:=^50}".format(""))
