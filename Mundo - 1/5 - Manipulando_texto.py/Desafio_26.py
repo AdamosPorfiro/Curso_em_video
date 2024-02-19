495996a6ce0729a40cfd0f710b61352e957bab47
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-19 11:42:25 -0300

    Desafio_26.py

--- Mundo - 1/5 - Manipulando_texto.py/Desafio_26.py
+++ Mundo - 1/5 - Manipulando_texto.py/Desafio_26.py
@@ -7,9 +7,12 @@ Faça um programa que leia uma frase pelo teclado e mostre:
 - Em que posição ela aparece a primeira vez
 - Em que posição ela apareçe a ultima vez
 """
+from time import sleep
 print("{:=^50}".format(" Desafio 26 "))
 frase = str(input("Digite um frase: ")).upper()
 frase = ''.join(frase.split())
+print("\nAnalisando a frase...")
+sleep(2.5)
 print("A letra 'A' apareçe {} vezes".format(frase.count("A")))
 print("A letra 'A' apareçe a 1° vez na posição {}".format(frase.find("A")+1))
 print("A letra 'A' apareçe uma ultima vez na posição {}".format(frase.rfind("A")+1))
