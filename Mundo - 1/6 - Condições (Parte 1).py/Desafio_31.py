867ad5160e6928e086b9d37979a766ae9b6c1af1
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-19 18:27:46 -0300

    Desafio_31.py

--- Mundo - 1/6 - Condições (Parte 1).py/Desafio_31.py
+++ Mundo - 1/6 - Condições (Parte 1).py/Desafio_31.py
@@ -7,3 +7,10 @@ Calcule o preço da passagem cobrando R$ 0,50 por km para viagens de até 200km
 e R$0,45 para viagens mais longas
 
 """
+print("{:=^50}".format(" Desafio 31 "))
+v_km = float(input("Informe a distância em km da sua viagem: "))
+if v_km <= 200:
+    print("\nO valor da passagem fica R$ {:.2f}".format(v_km*0.50))
+else:
+    print("\nO valor da passagem fica R$ {:.2f}".format(v_km*0.45))
+print("{:=^50}".format(""))
