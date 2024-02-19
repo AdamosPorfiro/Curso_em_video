0c81b75e464c77d2eec21fc628dc3fdf7d54856a
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-19 18:22:38 -0300

    Desafio_30.py

--- Mundo - 1/6 - Condições (Parte 1).py/Desafio_30.py
+++ Mundo - 1/6 - Condições (Parte 1).py/Desafio_30.py
@@ -3,3 +3,11 @@ Desafio 30
 
 Crie um programa que leia um numero inteiro qualquer e diga se ele é PAR ou IMPAR
 """
+
+print("{:=^25}".format(" Desafio 30 "))
+n = int(input("Digite um número: "))
+if n % 2 == 0:
+    print("\nO número {} é par".format(n))
+else:
+    print("\nO número {} é ímpar".format(n))
+print("{:=^25}".format(""))
