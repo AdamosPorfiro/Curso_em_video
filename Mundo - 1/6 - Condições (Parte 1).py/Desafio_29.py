55e3dffd7ebca7bbf12d5fea96d24c449caa69e8
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-19 18:15:54 -0300

    Desafio_29.py

--- Mundo - 1/6 - Condições (Parte 1).py/Desafio_29.py
+++ Mundo - 1/6 - Condições (Parte 1).py/Desafio_29.py
@@ -8,7 +8,10 @@ Escreva um programa que leia a velocidade de um carro:
 A multa ira custa R$ 7,00 por cada km acima do limite
 
 """
-
+print("{:=^45}".format(" Desafio 29 "))
 velocidade = float(input("Informe a velocidade do carro: "))
-if velocidade >= 80:
-    
+if velocidade > 80:
+    print("\nVocê levou uma multa no valor de R$ {:.2f}\nDirija com cuidado!".format((velocidade-80)*7))
+else:
+    print("\nAtenção, Dirija com cuidado!")
+print("{:=^45}".format(""))
