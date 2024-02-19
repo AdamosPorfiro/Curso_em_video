251c29591f057d70e3a2f98e27f6950c8014cc83
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-19 17:20:05 -0300

    Desafio_28.py

--- Mundo - 1/6 - Condições (Parte 1).py/Desafio_28.py
+++ Mundo - 1/6 - Condições (Parte 1).py/Desafio_28.py
@@ -6,3 +6,14 @@ tentar descobrir qual foi o numero escolhido pelo computador.
 
 O programa deverá escrever na tela se o usuario perdeu ou venceu!
 """
+
+from random import randint
+print("{:=^40}".format(" Desafio 28 "))
+n_aleatorio = (randint(0,5))
+print("Descubra o numero gerado pelo computador")
+usuario = int(input("Informe o número de 0 á 5: "))
+if usuario == n_aleatorio:
+    print("\nPARABÉNS, você venceu!!!")
+else:
+    print("\nVocê perdeu, tente novamente!")
+print("{:=^40}".format(""))
