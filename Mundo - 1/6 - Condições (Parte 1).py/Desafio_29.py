40854b2ef2a1b0fc5c5745499a570cdd58b5efab
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-19 17:57:36 -0300

    Desafio_29_incompleto.py

--- Mundo - 1/6 - Condições (Parte 1).py/Desafio_29.py
+++ Mundo - 1/6 - Condições (Parte 1).py/Desafio_29.py
@@ -8,3 +8,7 @@ Escreva um programa que leia a velocidade de um carro:
 A multa ira custa R$ 7,00 por cada km acima do limite
 
 """
+
+velocidade = float(input("Informe a velocidade do carro: "))
+if velocidade >= 80:
+    
