8c231ef801fea9272aeb5fd387152c1815520f14
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-20 11:49:38 -0300

    Desafio_33.py

--- Mundo - 1/6 - Condições (Parte 1).py/Desafio_33.py
+++ Mundo - 1/6 - Condições (Parte 1).py/Desafio_33.py
@@ -2,14 +2,21 @@
 Faça um programa que leia três numero e fale qual é o maior e qual é o menor
 """
 
-n_1 = int(input("Informe o 1° número: "))
-n_2 = int(input("Informe o 2° número: "))
-n_3 = int(input("Informe o 3° número: "))
-if n_1 > n_2 and n_2 > n_3:
-    print("\n{} é o maior número\n{} é o menor número".format(n_1,n_3))
-elif n_2 > n_1 and n_3 > n_1:
-    print("\n{} é o maior número\n{} é o menor número".format(n_2,n_1))
-elif n_3 > n_1 and n_1 > n_2:
-    print("\n{} é o maior número\n{} é o menor número".format(n_3,n_2))
+print("{:=^30}".format(" Desafio 33 "))
+n1 = int(input("Informe o 1° número: "))
+n2 = int(input("Informe o 2° número: "))
+n3 = int(input("Informe o 3° número: "))
+
+if n1>n2 and n2>n3:
+    print("{} é maior e {} é menor".format(n1,n3))
+elif n1>n3 and n3>n2:
+    print("{} é maior e {} é menor".format(n1,n2))
+elif n2>n1 and n1>n3:
+    print("{} é maior e {} é menor".format(n2,n3))
+elif n2>n3 and n3>n1:
+    print("{} é maior e {} é menor".format(n2,n1))
+elif n3>n1 and n1>n2:
+    print("{} é maior e {} é menor".format(n3,n2))
 else:
-    print("\n{} é o maior número\n{} é o menor número".format(n_1,n_2))
+    print("{} é maior e {} é menor".format(n3,n1))
+print("{:=^30}".format(""))
