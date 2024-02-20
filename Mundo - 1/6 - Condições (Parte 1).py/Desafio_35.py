deafd12c9f4a220f45a4837328eb984c4c6adb62
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-20 13:13:03 -0300

    Desafio_35.py

--- Mundo - 1/6 - Condições (Parte 1).py/Desafio_35.py
+++ Mundo - 1/6 - Condições (Parte 1).py/Desafio_35.py
@@ -5,3 +5,26 @@ Desenvolva um programa que leia o comprimento de 3 retas e vai dizer
 se ele pode ou não formar um triangulo
 
 """
+print("{:=^40}".format(" Desafio 35 "))
+r1 = float(input("Informe comprimento da 1º reta: "))
+r2 = float(input("Informe comprimento da 1º reta: "))
+r3 = float(input("Informe comprimento da 1º reta: "))
+
+
+if r1+r2>r3 and r1+r3>r2 and r2+r3>r1:
+    print("\nSim é possível formar um triângulo")
+else:
+    print("\nNão é possível formar um triângulo")
+print("{:=^40}".format(""))
+"""
+if r1+r2>r3:
+    if r1+r3>r2:
+        if r2+r3>r1:
+            print("\nPode sim formar um triângulo!")
+        else:
+            print("\nNão é possivel formar um triângulo")
+    else:
+        print("\nNão é possivel formar um triângulo")
+else:
+    print("\nNão é possivel formar um triângulo")
+"""
