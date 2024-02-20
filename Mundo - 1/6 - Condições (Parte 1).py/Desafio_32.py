66a324b0185051b994aec4707eacde230885f039
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-19 22:35:58 -0300

    Desafio_32.py

--- Mundo - 1/6 - Condições (Parte 1).py/Desafio_32.py
+++ Mundo - 1/6 - Condições (Parte 1).py/Desafio_32.py
@@ -2,4 +2,21 @@
 Desafio 32
 
 Faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO
+
+1° passo - Um ano é bissexto quando é divisivel por 4
+2° passo -  Se ele é divisivel por 100 e não por 400 não é bissexto
+3° passo -  Um ano que é divisivel por 400 ele é bissexto
 """
+print("{:=^40}".format(" Desafio 32 "))
+ano = int(input("Informe o ano que deseja verificar: "))
+if  ano%4 == 0:
+    if ano%100 == 0: 
+        if ano%400 == 0:
+            print("Esse ano é bissexto!")
+        else:
+            print("Esse ano não é bissexto!")
+    else:
+        print("Esse ano é bissexto!")
+else:
+    print("Esse ano não é bissexto!")
+print("{:=^40}".format(""))
