803dc5012f27cfaef3f873521aa7b571bed12284
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-20 12:50:56 -0300

    Desafio_34.py

--- Mundo - 1/6 - Condições (Parte 1).py/Desafio_34.py
+++ Mundo - 1/6 - Condições (Parte 1).py/Desafio_34.py
@@ -9,3 +9,12 @@ Escreva um programa que pergunte o salario de um funcionario e calcule o valor d
 - Para os inferiores ou iguais o aumento é de 15%
 
 """
+
+print("{:=^55}".format(" Desafio 34 "))
+salario = float(input("Informe o seu salario R$ "))
+if salario > 1250:
+    print("\nVocê terá um aumento de 10%' e agora receberá R$ {}".format(salario*10/100+salario))
+else:
+    print("\nVocê terá um aumento de 15%' e agora receberá R$ {}".format(salario*15/100+salario))
+
+print("{:=^55}".format(""))
