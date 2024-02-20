1a4668af5240f00dbb1a8c0778eed61668dcf60d
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-19 22:53:13 -0300

    Desafio_33_incompleto.py

--- Mundo - 1/6 - Condições (Parte 1).py/Desafio_33.py
+++ Mundo - 1/6 - Condições (Parte 1).py/Desafio_33.py
@@ -1,3 +1,15 @@
 """
 Faça um programa que leia três numero e fale qual é o maior e qual é o menor
 """
+
+n_1 = int(input("Informe o 1° número: "))
+n_2 = int(input("Informe o 2° número: "))
+n_3 = int(input("Informe o 3° número: "))
+if n_1 > n_2 and n_2 > n_3:
+    print("\n{} é o maior número\n{} é o menor número".format(n_1,n_3))
+elif n_2 > n_1 and n_3 > n_1:
+    print("\n{} é o maior número\n{} é o menor número".format(n_2,n_1))
+elif n_3 > n_1 and n_1 > n_2:
+    print("\n{} é o maior número\n{} é o menor número".format(n_3,n_2))
+else:
+    print("\n{} é o maior número\n{} é o menor número".format(n_1,n_2))
