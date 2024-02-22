4702ea9887b8f298e17390e4327220e878c4e6a1
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-22 12:22:01 -0300

    Desafio_2.py

--- Mundo - 1/1_Primeiros_passo.py/Desafio_2.py
+++ Mundo - 1/1_Primeiros_passo.py/Desafio_2.py
@@ -31,8 +31,16 @@ Pseudocódigo:
 
 Código final abaixo:
 '''
-print('===== Desafio 2 =====')
-dia = input('\nQual foi o dia do seu nascimento?\n') # 10
-mes = input('Qual foi o mês do seu nascimento?\n') # novembro
-ano = input('Qual foi o ano do seu nascimento?\n') # 1993
-print('Você nasceu no dia,',dia,mes, 'de',ano,'.Correto?') # Você nasceu no dia, 10 de novembro', de 1993. Correto?
+from datetime import datetime
+from emoji import emojize
+print(emojize('===== Desafio 2 - \33[0;32mUpado\33[m :dorso_da_mão_com_dedo_indicador_apontando_para_cima: =====',language='pt'))
+dia = int(input('\nQual foi o dia do seu nascimento?\n')) # 10
+mes = int(input('Qual foi o mês do seu nascimento?\n')) # novembro
+ano = int(input('Qual foi o ano do seu nascimento?\n')) # 1993
+
+if mes == datetime.today().month and dia <= datetime.today().day:
+    idade = datetime.today().year - ano
+else:
+    idade = datetime.today().year - ano - 1
+#print('Você nasceu no dia,',dia,mes, 'de',ano,' ') # Você nasceu no dia, 10 de novembro', de 1993. Correto?
+print(emojize(f"Você nasceu no dia {dia} do mes {mes} no ano de {ano} e tem atualmente {idade} anos :bebê:",language='pt'))
