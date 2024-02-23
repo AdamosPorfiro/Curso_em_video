0f30ac1b3b596793669a4d816643fd11026cd001
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-23 18:13:31 -0300

    Desafio_39.py

--- Mundo - 2/1 - Condições_aninhadas.py/Desafio_39.py
+++ Mundo - 2/1 - Condições_aninhadas.py/Desafio_39.py
@@ -9,7 +9,15 @@ Seu programa tambem deverá mostrar o tempo que falta ou que passou do prazo.
 
 '''
 
+from datetime import datetime
 nome = str(input("Nome: "))
 dia_nasceu = int(input("Informe o dia em que nasceu:  "))
 mes_nasceu = int(input("Informe o mês em que nasceu:  "))
 ano_nasceu = int(input("Informe o ano em que nasceu:  "))
+
+if datetime.today().year - ano_nasceu < 18:
+    print("\nVocê não completou 18 anos ainda, volte quando completar 18 anos.\nPara mais informações acesse: https://alistamento.eb.mil.br/")
+elif datetime.today().year - ano_nasceu == 18 and datetime.today().month <= mes_nasceu and datetime.today().day >= dia_nasceu:
+    print("\nParabéns, está na hora de você se alistar, acesse o site e se faça sua candidatura: https://alistamento.eb.mil.br/")
+elif datetime.today().year - ano_nasceu > 18:
+    print("\nVocê já passou do tempo de alistamento acesse o site para mais informações: https://alistamento.eb.mil.br/") 
