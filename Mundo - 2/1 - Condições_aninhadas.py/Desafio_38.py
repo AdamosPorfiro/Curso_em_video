c75b402f1857b13d332545d7acc78cdbf01d0927
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-22 21:15:27 -0300

    Desafio_38.py

--- Mundo - 2/1 - Condições_aninhadas.py/Desafio_38.py
+++ Mundo - 2/1 - Condições_aninhadas.py/Desafio_38.py
@@ -6,3 +6,16 @@ Escreva um programa que leia dois numeros inteiros e compare-os, mostrando na te
 - Não existe valor maior, os dois são iguais;
 
 '''
+print("-="*20)
+print("{:>30}".format("Comparando dois numeros"))
+print("-="*20)
+n1 = int(input("Informe o primeiro numero: "))
+n2 = int(input("Informe o segundo numero: "))
+
+if n1 > n2:
+    print("O primeiro valor é maior")
+elif n2 > n1:
+    print("O segundo valor é maior")
+else:
+    print("Não existe valor maior, os dois são iguais")
+print("="*40)
