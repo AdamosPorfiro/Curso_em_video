12bb3cc52dfd91149dcfd6687b592f6fb446996d
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-22 21:05:05 -0300

    Desafio_37.py

--- Mundo - 2/1 - Condições_aninhadas.py/Desafio_37.py
+++ Mundo - 2/1 - Condições_aninhadas.py/Desafio_37.py
@@ -7,3 +7,29 @@ de conversão:
 2 - para octal;
 3 - para hexadecimal;
 '''
+
+from time import sleep
+print("-="*27)
+print("{:>38}".format("Conversor de base númerica"))
+print("-="*27)
+n1 = int(input("Informe um numero inteiro: "))
+base = int(input(
+"""Qual a base de conversão que deseja:
+        1 - BINÁRIO
+        2 - OCTAL
+        3 - HEXADECIMAL
+        Digite: """))
+if base == 1:
+    base = "BINÀRIO"
+    n1 = bin(n1)
+elif base == 2:
+    base = "OCTAL"
+    n1 = oct(n1)
+elif base == 3:
+    base = "HEXADECIMAL"
+    n1 = hex(n1)
+print("Convertendo dados...")
+sleep(1.5)
+
+print(f"O número convertido para {base} fica {n1}")
+print("-="*27)
