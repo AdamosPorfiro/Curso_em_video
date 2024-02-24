e2df613beb27cf9a53b8a9b2f104f8776a7cf6a6
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-24 11:04:54 -0300

    Desafio_41.py

--- Mundo - 2/1 - Condições_aninhadas.py/Desafio_41.py
+++ Mundo - 2/1 - Condições_aninhadas.py/Desafio_41.py
@@ -9,3 +9,22 @@ de um atleta e mostre sua categoria, de acordo com a idade:
 - Acima: Master.
 
 '''
+print("-=" * 20)
+print("{:>25}".format(" Desafio 41 "))
+print("-=" * 20)
+from datetime import datetime
+ano_nasc = int(input("Informe o ano de nascimento: "))
+ano = datetime.today().year - ano_nasc
+
+if ano <= 9:
+    categoria = "MIRIM"
+elif ano > 9 and ano <= 14:
+    categoria = "INFATIL"
+elif ano > 14 and ano <= 19:
+    categoria = "JUNIOR"
+elif ano == 20:
+    categoria = "SÊNIOR"
+else:
+    categoria = "MASTER"
+print(f"Sua categoria é \33[1;32m{categoria}\33[m")
+print("-=" * 20)
