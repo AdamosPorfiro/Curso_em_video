7200637daea7d346f20b8634de66616fb80315dc
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-24 14:19:39 -0300

    Desafio_43.py

--- Mundo - 2/1 - Condições_aninhadas.py/Desafio_43.py
+++ Mundo - 2/1 - Condições_aninhadas.py/Desafio_43.py
@@ -9,3 +9,21 @@ o seu status, de acordo com a tabela abaixo:
 - Acima de 40: Obesidade mórbida;
 
 '''
+print("-="*20)
+print("\33[1;34m{:>29}\33[m".format(" Calculadora de IMC "))
+print("-="*20)
+peso = float(input("Informe o seu peso em kg: "))
+altura = float(input("Informa a sua altura: "))
+imc = (peso/altura)/altura
+
+if imc < 18.5:
+    print(f"O seu IMC é de {imc:.2f} e você está abaixo do peso")
+elif imc >= 18.5 and imc <= 25:
+    print(f"O seu IMC é de {imc:.2f} e você está no peso ideal")
+elif imc >= 25 and imc <= 30:
+    print(f"O seu IMC é de {imc:.2f} e você está com sobrepeso")
+elif imc >= 30 and imc <= 40:
+    print(f"O seu IMC é de {imc:.2f} e você está com obsidade")
+else:
+    print("Você está com obesidade mórbida")
+print("-="*20)
