a8dcd2b6bc451952130705e73146c331fa8e185e
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-24 17:21:28 -0300

    Desafio_45.py

--- Mundo - 2/1 - Condições_aninhadas.py/Desafio_45.py
+++ Mundo - 2/1 - Condições_aninhadas.py/Desafio_45.py
@@ -11,3 +11,40 @@ Regras:
 3 - Tesoura ganha do papel;
 
 '''
+
+from random import choice
+
+lista = ['Pedra', 'Papel', 'Tesoura']
+computador = choice(lista).lower()
+usuario = str(input("Escolha Pedra, Papel ou Tesoura: ")).strip().lower()
+
+# Empate
+if computador == 'pedra' and usuario == 'pedra':
+    print(f"Empatado!\nVocê escolheu {usuario} e o computador {computador}")
+elif computador == 'papel' and usuario == 'papel':
+    print(f"Empatado!\nVocê escolheu {usuario} e o computador {computador}")
+elif computador == 'tesoura' and usuario == 'tesoura':
+    print(f"Empatado!\nVocê escolheu {usuario} e o computador {computador}")
+
+
+#Vencedor computador
+elif computador == 'papel' and usuario == 'pedra':
+    print(f"Você perdeu!\nVocê escolheu {usuario} e o computador {computador}")
+elif computador == 'tesoura' and usuario == 'papel':
+    print(f"Você perdeu!\nVocê escolheu {usuario} e o computador {computador}")
+elif computador == 'pedra' and usuario == 'tesoura':
+    print(f"Você perdeu!\nVocê escolheu {usuario} e o computador {computador}")
+
+
+#Vencedor jogador
+elif computador == 'papel' and usuario == 'tesoura':
+    print(f"Parabéns, você ganhou!\nVocê escolheu {usuario} e o computador {computador}")
+elif computador == 'tesoura' and usuario == 'pedra':
+    print(f"Parabéns, você ganhou!\nVocê escolheu {usuario} e o computador {computador}")
+  
+elif computador == 'pedra' and usuario == 'papel':
+    print(f"Parabéns, você ganhou!\nVocê escolheu {usuario} e o computador {computador}")
+
+#Caso digite errado
+else:
+    print("Você deve digitar corretamente")
