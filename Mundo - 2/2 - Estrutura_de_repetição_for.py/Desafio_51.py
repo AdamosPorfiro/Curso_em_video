209b950c0f22d29012c4178086864917b36e915e
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-28 17:06:41 -0300

    Desafio_51.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_51.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_51.py
@@ -1,4 +1,26 @@
 '''
-Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final mostre os 10 primeiros termos  dessa progressão.
+Desenvolva um programa que leia o primeiro termo e a razão de uma PA. 
+No final mostre os 10 primeiros termos  dessa progressão.
 
+O que é o primeiro termo e a razão de uma PA
+R:. É uma sequencia de termos onde a diferença entre um termo e o seu anterior é sempre constante.
+Para escrever essa progressão precisamos da "primeiro termo" e da sua "razão" 
+
+Termo: 6 <-
+Razão: 3
+
+Primeiro termo é: 6
+Segundo termo é: 6+3 = 9, logo segundo termo é 9
+terceiro termo é: 12... 
 '''
+print("=-"*15)
+print("{:>20}".format(" Desafio 51 "))
+print("=-"*15)
+termo = int(input("Informe o termo: "))
+razao = int(input("Informe a razão: "))
+print(f"\nOs 10 primeiro são:\n1° termo {termo}")
+for c in range(2,11):
+    termo += razao
+    print(f"{c}° termo {termo}")
+print("=-"*15)
+
