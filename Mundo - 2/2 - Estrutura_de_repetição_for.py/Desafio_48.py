6333bb589e34d2dca2a581ba2c482abcbfc36476
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-28 12:33:18 -0300

    Desafio_48.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_48.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_48.py
@@ -1,4 +1,12 @@
 """
-Faça um programa que calcule todo os numeros impares que são multiplos de três e que se encontram no intervalo  de 1 até 500
+Faça um programa que calcule a soma de todos os numeros impares que são multiplos de três que se encontram no intervalo  de 1 até 500
 
 """
+print("-="*20)
+print("{:>25}".format(" Desafio 48 "))
+print("-="*20)
+soma = 0
+for c in range(1,501):
+    if c % 2 != 0:
+        soma += c
+print(f"A soma de todos os impares resulta no valor \33[1;32m{soma}\33[m")
