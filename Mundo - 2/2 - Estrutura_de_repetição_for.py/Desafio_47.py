d332ac5e46e81ba2d4b9b4bf672ecc10e729be78
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-28 12:26:57 -0300

    Desafio_47.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_47.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_47.py
@@ -5,7 +5,7 @@ Crie um programa que mostre na tela todos os numeros pares que estão no interva
 print("-="*5)
 print("Desafio 47")
 print("-="*5)
-for c in range(1,50):
+for c in range(1,51):
     if c % 2 == 0:
         print(c)
 print("-="*5)
