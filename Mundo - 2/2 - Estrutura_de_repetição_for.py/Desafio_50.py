372aa5a0b655b90ff951e3bde80cad871988d398
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-28 13:36:36 -0300

    Desafio_50.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_50.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_50.py
@@ -2,7 +2,14 @@
 Desenvolva um programa que leia 6 numeros inteiros, e mostre a soma apenas daqueles que forem pares, se o valor digitado for
 impar desconsidere-o
 
-
 '''
-        
-        
+
+soma = 0
+for c in range(1,7):
+    n = int(input(f"Informe {c}° número inteiro: "))
+    if n % 2 == 0:
+        soma += n
+if soma != 0:
+    print(f"\nA soma dos números pares é {soma}")
+else:
+    print("\nNão exite número pares para somá-los")
