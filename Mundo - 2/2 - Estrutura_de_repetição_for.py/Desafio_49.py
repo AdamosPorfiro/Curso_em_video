ab571a223bb86bfddca10701e7871ab9318a5134
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-28 12:54:48 -0300

    Desafio_49.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_49.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_49.py
@@ -1,4 +1,11 @@
 """
 Refaça o desafio 009, mostrando a tabuada de um numero que o usuario escolher, só que agora utilizando um laço for
 """
-
+print("-="*20)
+print("{:>25}".format(" Desafio 49 "))
+print("-="*20)
+n = int(input("Numero que deseja saber a tabuada: "))
+for c in range(1,11):
+    mult = c*n
+    print(f"{c:2} x {n} = {mult}")
+print("-="*20)
