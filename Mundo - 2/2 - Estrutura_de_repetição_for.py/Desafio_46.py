ec82d7f391d5b1b83ccf59d4d3bc638f700b677c
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-28 12:17:21 -0300

    Desafio_46.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_46.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_46.py
@@ -10,7 +10,9 @@ from PIL import Image
 for c in range(10,-1,-1): #Vai fazer a contagem tirando 1
     print(c)
     sleep(1)
-
+print("-="*6)
+print("BoOoOoOmMmM")
+print("-="*6)
 # Carrego a imagem
 imagem = Image.open("C:\\Users\\Ideal\\Documents\\GitHub\\Curso_em_video\\Curso_em_video\\Mundo - 2\\2 - Estrutura_de_repetição_for.py\\fogos_.gif")
 #Abre a imagem
