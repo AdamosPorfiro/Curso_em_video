5853acf371238980ff7805dd2ce2069d8f801c63
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-29 17:53:13 -0300

    Desafio_56.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_56.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_56.py
@@ -6,4 +6,30 @@ Desenvolva um programa que leia, nome, idade e sexo de quatro pessoas. No final 
 - E quantas mulheres tem menos de 20 anos;
 
 '''
+media = 0
+mais_velho = 0
+nome_mais_velho = " "
+qtd_mulheres_menor_de_20 = 0
+for c in range(1,5):
+    nome = str(input("Nome: ")).strip().upper()
+    idade = int(input("Idade: "))
+    sexo = str(input("Sexo | M | F |: ")).strip().upper()
+    
+    # Média
+    media = media+idade
 
+    # Nome do homem mais velho
+    if sexo in "M":
+        if idade > mais_velho:
+            mais_velho = idade
+            nome_mais_velho = nome
+    #Quantas mulheres tem menos de 20 anos
+    if sexo in "F":
+        if idade < 20:
+            qtd_mulheres_menor_de_20 += 1
+print(f"A média de idade desse grupo é {media / 4}")
+if mais_velho != 0:
+    print(f"O homem mais velho tem {mais_velho} e o seu nome é: {nome_mais_velho.capitalize()}")
+else:
+    print("Não há homens nesse grupo")
+print(f"Quantidade de mulheres com menos de 20 anos {qtd_mulheres_menor_de_20}")
