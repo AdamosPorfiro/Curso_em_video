06903f8c89a487a7ad4b19916e23cad03329ecba
Adamos Porfiro <adamos.contato@gmail.com>
2024-02-29 17:20:48 -0300

    Desafio_55.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_55.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_55.py
@@ -2,3 +2,19 @@
 Faça um programa que leia o peso de 5 pessoas. No final mostre qual é o maior e o menor peso lidos.
 
 '''
+print("-="*20)
+print("{:>25}".format(" Desafio 55 "))
+print("-="*20)
+maior = 0
+menor = 0
+for c in range(1,6):
+    peso = float(input("Informe o seu peso: "))
+    if peso > maior:
+       maior = peso
+    if menor == 0:
+        menor = peso
+    if peso < menor:
+        menor = peso
+print(f"o maior peso é de {maior} kg")
+print(f"o menor peso é de {menor} kg")
+print("-="*20)
