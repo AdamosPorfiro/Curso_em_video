d38a638ca88fc68c4f3aaf4bd2f738940dbd579f
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-29 22:51:10 -0300

    Desafio_52.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_52.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_52.py
@@ -2,3 +2,16 @@
 Faça um programa que leia um numero inteiro e diga se ele é ou não um numero primo.
 
 """
+
+n = int(input("Informe um numero inteiro: "))
+
+divisor = 0
+
+for c in range(1, n+1):
+    if n % c == 0: # Se o numero informado for divisivel por 3 numeros ele não é primo
+        divisor += 1
+if divisor >= 3:
+    print("Esse número não é primo")
+else:
+    print(f"Sim, {n} é um número primo")
+    
