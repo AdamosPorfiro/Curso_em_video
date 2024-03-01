e66f664278c81b7eb4ad8e176fba0e571566ecae
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-29 23:04:36 -0300

    Desafio_47.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_47.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_47.py
@@ -2,10 +2,10 @@
 Crie um programa que mostre na tela todos os numeros pares que estão no intervalo entre 1 e 50.
 
 """
-print("-="*5)
-print("Desafio 47")
-print("-="*5)
+print("-="*35)
+print("{:>40}".format(" Desafio 47 "))
+print("-="*35)
 for c in range(1,51):
     if c % 2 == 0:
-        print(c)
-print("-="*5)
+        print(c,end=' ')
+print("\n"+"-="*35)
