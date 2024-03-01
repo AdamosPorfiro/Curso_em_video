0eb67815cf39b333e787627b2c186d060972fc40
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-02-29 23:09:41 -0300

    Desafio_48.py

--- Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_48.py
+++ Mundo - 2/2 - Estrutura_de_repetição_for.py/Desafio_48.py
@@ -5,8 +5,13 @@ Faça um programa que calcule a soma de todos os numeros impares que são multip
 print("-="*20)
 print("{:>25}".format(" Desafio 48 "))
 print("-="*20)
+
 soma = 0
+qtd = 0
+
 for c in range(1,501):
     if c % 2 != 0:
-        soma += c
-print(f"A soma de todos os impares resulta no valor \33[1;32m{soma}\33[m")
+        if c % 3 == 0:
+            soma += c
+            qtd += 1
+print(f"A soma de todos os {qtd} resulta no valor \33[1;32m{soma}\33[m")
