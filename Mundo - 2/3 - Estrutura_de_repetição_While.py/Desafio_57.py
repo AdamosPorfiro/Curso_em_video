22fb1367c8a8eb27e75a29acb0c5cb7f9aecec1a
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-02 17:35:54 -0300

    Desafio_57.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_57.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_57.py
@@ -2,3 +2,9 @@
 Faça um programa que leia o sexo de um pessoa, mas que só aceite M ou F. 
 Caso esteja errado, peça a digitação novamente até ter um valor correto;
 '''
+sexo = ' '
+while sexo not in 'MF':
+    sexo = str(input("Qual o seu sexo [M | F]: ")).upper()
+    if sexo not in 'MF':
+        print("OPS, erro!!! Digite corretamente!")
+print("Acabou!")
