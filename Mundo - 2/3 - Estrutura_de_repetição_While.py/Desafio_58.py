c60b4e10fa1d9c5472268e575005d2d9d5955ae7
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-02 18:07:59 -0300

    Desafio_58.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_58.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_58.py
@@ -4,5 +4,20 @@ Só que agora o jogador vai tentar adivinhar até, acertar, mostrando no final q
 palpites foram necessarios para vencer.
 '''
 
+from random import randint
 
+#Escolha do computador
+computador = randint(0,10)
 
+Qtd_palpites = jogador = 0
+
+while jogador != computador:
+    #Escolha jogador
+    jogador = int(input("Adivinhe o númnero sorteado pelo computador [0 á 10]: "))
+    Qtd_palpites += 1
+    if jogador > computador:
+        print("Pensei em um número menor\n")
+    elif jogador < computador:
+        print("Pensei em um número maior\n")
+#Venceu
+print(f"\nParabens, pensei no número {computador}\nVocê fez {Qtd_palpites} palpites")
