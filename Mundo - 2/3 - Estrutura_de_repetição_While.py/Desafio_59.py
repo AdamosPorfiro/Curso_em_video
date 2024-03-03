91b0638bb30d57f39f23a0479e7f3fd78882fdf7
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-03-03 15:18:32 -0300

    Desafio_59.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_59.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_59.py
@@ -9,43 +9,49 @@ Crie um programa que leia dois valores e mostre um menu na tela:
 '''
 from time import sleep
 opções = 0
+n1 = n2 = 0
 print("="*30)
 while opções != 5: 
     if opções == 4:
         while opções == 4:
             n1 = int(input("Digite o 1° valor: "))
             n2 = int(input("Digite o 2° valor: "))
+            opções = 0
             break
     else:
         n1 = int(input("Digite o 1° valor: "))
         n2 = int(input("Digite o 2° valor: "))
-    
-    print("""
-        [1] Somar
-        [2] Multiplicar
-        [3] Maior
-        [4] Novos números
-        [5] Sair do programa
-            """)
-    opções = int(input("Escolha: "))
-    #Somar
-    if opções == 1:
-        print(f"A soma dos valores {n1} + {n2} = {n1+n2}")
-    #Multiplicação
-    elif opções == 2:
-        print(f"A multiplicação dos valores {n1} x {n2} = {n1*n2}")
-    #Maior
-    elif opções == 3:
-        if n1 > n2:
-            print(f"O maior número é o 1° valor: {n1}")
-        else:
-            print(f"O maior número é o 2° valor: {n2}")
-    #Novos números
-    elif opções == 4:
-        print("Informe novos valores!")
-    #Sair do programa
-    elif opções == 5:
-        print("Saindo do programa...")
-        sleep(1)
-        print("Obrigado, Volte sempre!")
-    print("="*30)
+    while opções == 0 or opções == 1 or opções == 2 or opções == 3:  
+        print("""
+            [1] Somar
+            [2] Multiplicar
+            [3] Maior
+            [4] Novos números
+            [5] Sair do programa
+                """)
+        opções = int(input("Escolha: "))
+        #Somar
+        if opções == 1:
+            soma = n1 + n2
+            print(f"A soma dos valores {n1} + {n2} = {soma}")
+            #print(f"A soma dos valores {n1} + {n2} = {n1+n2}")
+        #Multiplicação
+        elif opções == 2:
+            multiplicação = n1 * n2
+            print(f"A multiplicação dos valores {n1} x {n2} = {multiplicação}")
+            #print(f"A multiplicação dos valores {n1} x {n2} = {n1*n2}")
+        #Maior
+        elif opções == 3:
+            if n1 > n2:
+                print(f"O maior número é o 1° valor = {n1}")
+            else:
+                print(f"O maior número é o 2° valor = {n2}")
+        #Novos números
+        if opções == 4:
+            print("Informe novos valores!")
+        #Sair do programa
+        elif opções == 5:
+            print("Saindo do programa...")
+            sleep(1)
+            print("Obrigado, Volte sempre!")
+        print("="*30)
