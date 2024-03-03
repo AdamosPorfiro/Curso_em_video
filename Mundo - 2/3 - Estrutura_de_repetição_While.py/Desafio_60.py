2fa039b329dc43cee0c52faf6af3d34b3305eaba
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-03-03 20:17:21 -0300

    Desafios_60.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_60.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_60.py
@@ -3,7 +3,9 @@ Faça um programa que leia um numero qualquer e mostre o seu fatorial.
 
 Ex: 5! = 5x4x3x2x1= 120
 """
-
+print("-="*20)
+print("{:>25}".format(" Desafio 60 "))
+print("-="*20)
 n = int(input("Informe o numero para receber o fatorial: ")) + 1
 fatorial = 1
 while n != 1:
