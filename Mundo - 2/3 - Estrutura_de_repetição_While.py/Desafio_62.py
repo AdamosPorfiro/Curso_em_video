db8fe481daec4b260ef947e67cad850c0d07f172
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-04 11:52:06 -0300

    Desafio_62.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_62.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_62.py
@@ -8,14 +8,17 @@ print("-="*20)
 
 termo = int(input("Informe o termo: "))
 razão = int(input("Informe a razão: "))
-sair = 0
-pa = termo
-
-while sair < 10:
-    pa += razão
-    sair += 1
-    print(f"{pa}", end= ' ⮕ ')
-mais_termos = int(input("\nDeseja ver mais termos? Quantos, se deseja sair digite 0:  "))
+pa = int(input("Informe quantos termos deseja ver: "))
+calculo = termo
+mais_termos = 1
+while mais_termos != 0:
+    while pa != 0:
+        calculo += razão
+        pa -= 1
+        print(f"{calculo-razão}", end= ' ⮕ ')
+    print("Acabou")
+    mais_termos = int(input("\nDeseja ver mais termos? Quantos, se deseja sair digite 0:  "))
+    pa = mais_termos
 
 
 
