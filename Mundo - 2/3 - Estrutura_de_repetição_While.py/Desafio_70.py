f493c15a167fdd2d1bd98ccc1a136ebe51423843
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-06 17:13:08 -0300

    Desafios.py

---
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_70.py
@@ -0,0 +1,10 @@
+'''
+Crie um programa que leia o nome e o preço de varios produtos.
+O programa deverá perguntar se o usuario vai continuar.
+No final, mostrar:
+
+A - Qual é o total gasto na compra
+B - Quantos produtos custam mais que R$ 1000
+C - Qual é o nome do produto mais barato
+
+'''
---
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_71.py
@@ -0,0 +1,14 @@
+'''
+Crie um programa que simule o funcionamento de um caixa eletronico.
+No inicio, pergunte ao usuario qual será o valor a ser sacado (numero inteiro)
+e o programa vai dizer quantas cedulas de cada valor serão entregues.
+
+OBS: Considere que  o caixa possui cedulas de:
+R$50 R$20 R$10 e R$1
+'''
+
+
+
+
+
+
