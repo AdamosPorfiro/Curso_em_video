27166b3dc9aa409e6825465dce781a9540c9fce9
Adamos Porfiro <110059432+AdamosPorfiro@users.noreply.github.com>
2024-03-06 22:01:21 -0300

    Desafio_68_incompleto.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_68.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_68.py
@@ -3,3 +3,8 @@ Faça um programa que o usuario jogue PAR ou ÍMPAR com o computador.
 O jogo só será interronpido quando o jogador perder, mostrando o total de vitórias
 consecutivas que ele conquistou no final do jogo.
 '''
+
+from random import randint
+while True:
+    computador = randint(1,10)
+    
