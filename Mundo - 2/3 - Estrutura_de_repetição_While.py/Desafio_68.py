85ae632679c63b7b5033951a57f71141801f8ee3
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-07 15:47:56 -0300

    Desafio_68.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_68.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_68.py
@@ -3,9 +3,10 @@ Faça um programa que o usuario jogue PAR ou ÍMPAR com o computador.
 O jogo só será interronpido quando o jogador perder, mostrando o total de vitórias
 consecutivas que ele conquistou no final do jogo.
 '''
-
+from time import sleep
 from random import randint,choice
 escolha_computador = ['I','P']
+Jogadas = empates = 0
 while True:
 
     #Computador
@@ -36,17 +37,24 @@ while True:
 
     #Resultado
     print(f"Computador escolheu o número {computador} e o jogador {jogador} a soma fica {soma}\nO número {soma} é {resultado}")
-    print(f"O jogador escolheu {escolha_computador} e o computador escolheu {escolha_jogador}")
+    print(f"O jogador escolheu {escolha_jogador} e o computador escolheu {escolha_computador}")
 
     #Computador vende
     if escolha_computador == resultado and escolha_jogador != resultado:
+        print(f"Venceu {Jogadas} vezes e Empatou {empates} vezes")
         print("Você perdeu! GAME OVER")
-        continuar = str(input("Quer tentar novamente [S | N]: "))
+        continuar = str(input("Quer tentar novamente [S | N]: ")).strip().upper()
         if continuar in 'N':
             break
     #Jogador vence
     elif escolha_jogador == resultado and escolha_computador != resultado:
-        print("Parabens, Você VENCEU!")
+        Jogadas +=1
+        print(f"Parabens, Você VENCEU {Jogadas} vezes e empatou {empates} vezes")
     #Empate
     elif escolha_computador == resultado and escolha_jogador == resultado:
-        print("Empatado")
+        empates += 1
+        print(f"Empatado, Você empatou {empates} vezes")
+    else:
+        print("Ninguem ganhou!!!")
+print("Fechando o jogo...")
+sleep(1.5)
