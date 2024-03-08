df361ffc65de0b9ce7f648cf2b36eda6295920f8
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-08 17:40:35 -0300

    Desafio_69.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_69.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_69.py
@@ -6,22 +6,37 @@ A - Quantas pessoas tem mais de 18 anos
 B - Quantos homens foram cadastrados
 C - Quantas mulheres tem menos de 20 anos
 '''
-
+maior_de_18 = homens_cadastrados = mulheres_menos_de_20 = 0
 while True:
     print("-="*25)
     idade = int(input("Informe a idade: "))
     sexo = str(input("Sexo [M | F]: ")).strip().upper()
+    #cont = str(input("Deseja continuar [S | N]: ")).strip().upper()
+
+    #Dados de saída
+    if idade >= 18:
+        maior_de_18 += 1
+    if sexo in 'M':
+        homens_cadastrados += 1
+    if sexo in 'F' and idade < 18:
+        mulheres_menos_de_20 += 1
+
+    #Deseja continuar
     while True:
-        continuar = str(input("Deseja continuar [S | N]: ")).strip().upper()
-        if continuar in 'S':
+        cont = str(input("Deseja continuar [S | N]: ")).strip().upper()
+        if cont in 'S':
             break
-        elif continuar in 'N':
+        elif cont in 'N':
             break
-        print("-="*25)
+    if cont in 'N':
         break
-    if continuar in 'N':
-        break
+
+#Imprimir os dados e saída
+print(f"{maior_de_18} Pessoas são maiores de 18 anos")
+print(f"{homens_cadastrados} homens foram cadastrados")
+print(f"{mulheres_menos_de_20} mulheres tem menos de 20 anos")
     
+        
 
     
             
