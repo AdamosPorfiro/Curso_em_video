2b1a3553e36c8a4bdfce1d25d4aac7de3b25250e
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-09 17:16:04 -0300

    Desafio_70.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_70.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_70.py
@@ -9,16 +9,21 @@ C - Qual é o nome do produto mais barato
 
 '''
 total_gasto = mais_de_mil = preço_prod_mais_barato = 0
-produto_mais_barato = ''
+produto_mais_barato = ' '
+
 while True:
+    print("="*30)
     nome_prod = str(input("Nome do produto: "))
     preço_prod = float(input("Preço do produto: "))
+    print("="*30)
     total_gasto += preço_prod
     if preço_prod > 1000:
         mais_de_mil += 1
+    preço_prod_mais_barato = preço_prod
+    produto_mais_barato = nome_prod
 
     #Produto mais barato
-    if preço_prod < preço_prod_mais_barato:
+    if preço_prod_mais_barato < preço_prod:
         preço_prod_mais_barato = preço_prod
         produto_mais_barato = nome_prod
     
@@ -31,6 +36,7 @@ while True:
             break
     if cont in 'N':
         break
+print("-"*50)
 print(f"Total gasto R$ {total_gasto}")
 print(f"{mais_de_mil} produtos custaram mais de R$ 1000")
 print(f"O nome do produto mais barato é {produto_mais_barato} custando R$ {preço_prod_mais_barato}")
