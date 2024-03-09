82d976759b40ae8b955eb2e456b07725d2926e35
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-08 21:29:58 -0300

    Desafio_70.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_70.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_70.py
@@ -8,3 +8,31 @@ B - Quantos produtos custam mais que R$ 1000
 C - Qual é o nome do produto mais barato
 
 '''
+total_gasto = mais_de_mil = preço_prod_mais_barato = 0
+produto_mais_barato = ''
+while True:
+    nome_prod = str(input("Nome do produto: "))
+    preço_prod = float(input("Preço do produto: "))
+    total_gasto += preço_prod
+    if preço_prod > 1000:
+        mais_de_mil += 1
+
+    #Produto mais barato
+    produto_mais_barato = nome_prod
+    preço_prod_mais_barato = preço_prod
+    if preço_prod < preço_prod_mais_barato:
+        preço_prod_mais_barato = preço_prod
+        produto_mais_barato = nome_prod
+    
+    #Continuar ou sair
+    while True:
+        cont = str(input("Quer continuar [S | N]: ")).strip().upper()
+        if cont in 'S':
+            break
+        elif cont in 'N':
+            break
+    if cont in 'N':
+        break
+print(f"Total gasto R$ {total_gasto}")
+print(f"{mais_de_mil} produtos custaram mais de R$ 1000")
+print(f"O nome do produto mais barato é {produto_mais_barato} custando R$ {preço_prod_mais_barato}")
