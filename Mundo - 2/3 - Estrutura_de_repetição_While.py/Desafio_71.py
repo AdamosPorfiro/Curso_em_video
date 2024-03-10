0cc28f7590fff43154bdf03c700368b09f6cabba
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-09 23:30:41 -0300

    Desafio_71_incompleto.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_71.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_71.py
@@ -6,6 +6,32 @@ e o programa vai dizer quantas cedulas de cada valor serão entregues.
 OBS: Considere que  o caixa possui cedulas de:
 R$50 R$20 R$10 e R$1
 '''
+from time import sleep
+print("-="*15)
+print("{:>20}".format("Adamos bank"))
+print("-="*15)
+nota_1 = nota_10 = Nota_20 = 0
+
+while True:
+    print("\nCaixa possui notas de: R$50 | R$20 | R$10 | R$1")
+    saque = int(input("Qual o valor a ser sacado R$ "))
+"""
+    # Saque de 1 a 9
+    if saque <= 9:
+        for c in range(saque,0,-1):
+            nota_1 += 1
+            if c == 0:
+                break
+        if nota_1 == saque:
+            break
+"""
+    
+print("\nSacando...")
+sleep(0.5)
+if saque <= 9:
+    print(f"Você vai receber {nota_1} notas de R$ 1")
+if saque >= 10 and saque <= 49:
+    print(f"Você vai receber {nota_10} notas de R$ 1")
 
 
 
