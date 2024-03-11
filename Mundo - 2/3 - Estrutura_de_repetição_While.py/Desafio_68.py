e80f07db1a70929eef0c157ccdc0a3e72888d5e5
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-11 19:50:44 -0300

    Desafio_68.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_68.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_68.py
@@ -45,28 +45,29 @@ while True:
         resultado = "ÍMPAR"
 
     #Resultado
-    print(f"Computador escolheu o número {computador} e o jogador {jogador}")
+    print(f"Computador jogou {computador} e o jogador {jogador}")
     print(f"A soma fica {computador} + {jogador} = {soma}\nO número {soma} é {resultado}")
     print("~"*30)
     print(f"O jogador escolheu {escolha_jogador} e o computador escolheu {escolha_computador}")
     print("~"*30)
 
-    #Computador vende
+    #Computador vence
     if escolha_computador == resultado and escolha_jogador != resultado:
         print(f"Venceu {Jogadas} vezes e Empatou {empates} vezes")
-        print("Você perdeu! GAME OVER")
+        print("Você perdeu! \33[1;31mGAME OVER\33[m")
         continuar = str(input("Quer tentar novamente [S | N]: ")).strip().upper()
         if continuar in 'N':
             break
     #Jogador vence
     elif escolha_jogador == resultado and escolha_computador != resultado:
         Jogadas +=1
-        print(f"Parabens, Você VENCEU {Jogadas} vezes e empatou {empates} vezes")
+        print(f"Parabéns, Você \33[1;32mVENCEU\33[m {Jogadas} vezes e empatou {empates} vezes")
     #Empate
     elif escolha_computador == resultado and escolha_jogador == resultado:
         empates += 1
-        print(f"Empatado, Você empatou {empates} vezes")
+        print(f"\33[1;33mEMPATADO\33[m, Você empatou {empates} vezes")
     else:
         print("Ninguem ganhou!!!")
 print("Fechando o jogo...")
 sleep(1.5)
+print("Obrigado por jogar, \33[1;32mVOLTE SEMPRE!\33[m")
