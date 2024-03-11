f2d33972c705cc35ff0bc61b7f347ea6cf88b9af
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-11 19:37:55 -0300

    Desafio_67.py

--- Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_67.py
+++ Mundo - 2/3 - Estrutura_de_repetição_While.py/Desafio_67.py
@@ -3,11 +3,14 @@ Faça um programa que faça a tabuada de varios numeros, um de cada vez, para ca
 O programa será interronpido quando o valor digitado for negativo.
 '''
 while True:
-    n = int(input("Informe número para tabuada: "))
+    print("="*50)
+    n = int(input("Quer ver a tabuada de qual valor: "))
     for c in range(1,11):
         if n < 0:
             break
         else:
             print(f"{c:2} x {n} = {c*n:2}")
     if n < 0:
+        print("="*50)
         break
+print(f"Programa de tabuada \33[1;31mENCERRADO\33[m Volte sempre!!!")
