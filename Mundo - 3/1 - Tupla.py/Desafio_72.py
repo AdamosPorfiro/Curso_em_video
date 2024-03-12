4720da727ba49d6229c1cc8485711fa3481447b5
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-12 17:32:57 -0300

    Desafio_72.py

--- Mundo - 3/1 - Tupla.py/Desafio_72.py
+++ Mundo - 3/1 - Tupla.py/Desafio_72.py
@@ -2,3 +2,16 @@
 Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero
 até vinte. Seu programa deverá ler um numero pelo teclado(entre 0 e 20) e mostra-lo por extenso.
 '''
+
+contagem_por_extenso_0_a_20 = (
+    'zero','um','dois','três','quatro','cinco','seis','sete','oito','nove','dez',
+    'onze','doze','treze','quatorze','quinze','dezesseis','dezessete','dezoito','dezenove','vinte'
+                              )
+while True:
+    numero = int(input("Digite um número: "))
+    for pos,num in enumerate(contagem_por_extenso_0_a_20):
+        if numero == pos:
+            print(f"{num}")
+    continuar = str(input("Deseja continuar [S | N]: ")).strip().upper()
+    if continuar == 'N':
+        break
