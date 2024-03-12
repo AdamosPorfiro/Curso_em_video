1ab13d3b7b0e20b790377a407d180ac5c8abe54a
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-11 21:00:05 -0300

    Desafio.py

---
+++ Mundo - 3/1 - Tupla.py/Desafio_72.py
@@ -0,0 +1,4 @@
+'''
+Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero
+até vinte. Seu programa deverá ler um numero pelo teclado(entre 0 e 20) e mostra-lo por extenso.
+'''
---
+++ Mundo - 3/1 - Tupla.py/Desafio_74.py
@@ -0,0 +1,6 @@
+'''
+Crie um programa que vai gerar 5 numeros aleatórios e colocar em uma tupla.
+
+Depois disso, mostre a listagem de números gerados 
+e também indique o menor e o maior valor que estão da tupla
+'''
