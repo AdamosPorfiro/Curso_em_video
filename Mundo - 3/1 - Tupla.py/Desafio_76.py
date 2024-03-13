d8c1750b67a0768ef74a25b628627bc085573381
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-13 20:40:28 -0300

    Desafio_76_incompleto.py

--- Mundo - 3/1 - Tupla.py/Desafio_76.py
+++ Mundo - 3/1 - Tupla.py/Desafio_76.py
@@ -5,3 +5,9 @@ na sequência.
 No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 
 '''
+
+produtos_com_valores = (
+    'Caneta', 2.00, 'Lapis', 2.50, 'Apontador', 4.00, 'Borracha', 2.50, 'Tesoura', 5.00, 'Caderno', 28.00,
+    'Mochila Escolar', 99.90, 'Cartolina', 2.00, 'Lapis de cor', 25.00, 'Caneta colorida', 8.50
+                        )
+
