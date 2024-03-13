5cecfa64ca4e0b237efc09155a5339ef13be391b
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-12 21:14:55 -0300

    Desafio_73.py

--- Mundo - 3/1 - Tupla.py/Desafio_73.py
+++ Mundo - 3/1 - Tupla.py/Desafio_73.py
@@ -8,3 +8,27 @@ C - Uma lista com os times em ordem alfabética.
 D - Em que posição na tabela está o time da Chapecoense.
 
 '''
+
+tabela_times = (
+    'Athletico-PR', 'Atlético-GO', 'Atlético-MG', 'Bahia','Botafogo', 'Cortinthians',
+    'Criciúma', 'Cruzeiro', 'Cuiabá', 'Flamengo', 'Fluminense', 'Fortaleza', 'Grêmio', 'Internacional',
+    'Juventude', 'Palmeiras', 'Bragantino', 'São paulo', 'Vasco da gama', 'Chapecoense'
+)
+print(f"Os 5 primeiros colocados\n")
+for pos, time in enumerate(tabela_times[0:5]):
+    print(f"{pos+1}° {time}")
+print("-="*15)
+print("Of últimos 4 colocados\n")
+for pos, time in enumerate(tabela_times[16:21]):
+    print(f"{pos+1}° {time}")
+print("-="*15)
+print("Tabela em ordem alfabética\n")
+print(sorted(tabela_times))
+print("-="*15)
+print("Chapecoense está na posição\n")
+for pos,time in enumerate(tabela_times):
+    if time in 'Chapecoense':
+        print(f"{pos+1}° {time}")
+print("-="*15)
+
+    
