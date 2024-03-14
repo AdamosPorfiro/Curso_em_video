4cbbf24328a41f8e8a9aeb970304430649d36234
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-14 20:45:17 -0300

    Desafio_76.py

--- Mundo - 3/1 - Tupla.py/Desafio_76.py
+++ Mundo - 3/1 - Tupla.py/Desafio_76.py
@@ -10,16 +10,10 @@ produtos_com_valores = (
     'Caneta', 2.00, 'Lapis', 2.50, 'Apontador', 4.00, 'Borracha', 2.50, 'Tesoura', 5.00, 'Caderno', 28.00,
     'Mochila Escolar', 99.90, 'Cartolina', 2.00, 'Lapis de cor', 25.00, 'Caneta colorida', 8.50
 )
-<<<<<<< HEAD
-palavra = ''
-for pos, item in enumerate(produtos_com_valores[0::2]):
-    print(f"{item:<20}......", end=' R$')
-    print(" 2")
-=======
-
-for iten in range(0, len(produtos_com_valores),2):
-    produtos_sem_valor = produtos_com_valores[iten]
-    print(f"{produtos_sem_valor}")
-    print("{:>20}".format("R$"))
-        
->>>>>>> 97c9abf8f3899385538e01a9cffabb993237f1aa
+print("="*40)
+print("="*40)
+print("Produtos {:>30}".format(" Preços "))
+print("-"*40)
+#Função zip() combina os produtos com seus respectivos preços e então imprimi cada par formatado adequadamente
+for produtos, preços in zip(produtos_com_valores[0::2], produtos_com_valores[1::2]):
+    print("{:<15}..................R$ {:4}".format(produtos, preços))
