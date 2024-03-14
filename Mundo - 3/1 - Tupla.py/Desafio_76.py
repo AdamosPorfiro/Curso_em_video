41c6f0d9c2aeab4e78f2122546fd839de34c3941
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-13 22:39:58 -0300

    Desafio_76.py

--- Mundo - 3/1 - Tupla.py/Desafio_76.py
+++ Mundo - 3/1 - Tupla.py/Desafio_76.py
@@ -10,15 +10,7 @@ produtos_com_valores = (
     'Caneta', 2.00, 'Lapis', 2.50, 'Apontador', 4.00, 'Borracha', 2.50, 'Tesoura', 5.00, 'Caderno', 28.00,
     'Mochila Escolar', 99.90, 'Cartolina', 2.00, 'Lapis de cor', 25.00, 'Caneta colorida', 8.50
 )
-
-print("Listagem de Preços:")
-print("=" * 30)
-print("{:<20} {:>10}".format("Produto", "Preço"))
-print("-" * 30)
-
-for i in range(0, len(produtos_com_valores), 2):
-    produto = produtos_com_valores[i]
-    preco = produtos_com_valores[i + 1]
-    print("{:<20} R$ {:>8.2f}".format(produto, preco))
-
-        
+palavra = ''
+for pos, item in enumerate(produtos_com_valores[0::2]):
+    print(f"{item:<20}......", end=' R$')
+    print(" 2")
