65d65823e5a2856adef71187d9c572bb5eb4421b
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-15 22:47:06 -0300

    Desafio_81_incompleto.py

--- Mundo - 3/2 - Lista_part_1.py/Desafio_81.py
+++ Mundo - 3/2 - Lista_part_1.py/Desafio_81.py
@@ -9,3 +9,29 @@ Crie um programa que vai ler varios numeros e colocar em uma lista.
 
  C) Se o valor 5 foi digitado e está ou não na lista
 '''
+numeros = []
+qtd_numeros = numero_5 = 0
+while True:
+    n = int(input("Digite um número: "))
+    numeros.append(n)
+    numero_5 += 1
+    qtd_numeros += 1
+    while True:
+        sair = str(input("Deseja sair [S|N]: ")).strip().upper()
+        if sair not in 'SN':
+            continue
+        elif sair in 'S':
+            break
+        elif sair in 'N':
+            break
+    if sair in 'N':
+        break
+print(f"Foram digitados {qtd_numeros} números")
+numeros.sort(reverse=True)
+print(numeros)
+for pos, item in enumerate(numeros):
+    if item[pos] == '5':
+        print(f"Sim, o número 5 está na lista na posição {pos}")
+        break
+    else:
+        print("Não, o número 5 não está na lista")
