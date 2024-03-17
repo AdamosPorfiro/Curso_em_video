3bbfb380a87dfd31a4896588ad6b54a4654d37c3
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-16 21:19:16 -0300

    Desafio_82.py

--- Mundo - 3/2 - Lista_part_1.py/Desafio_82.py
+++ Mundo - 3/2 - Lista_part_1.py/Desafio_82.py
@@ -6,3 +6,29 @@ respectivamente.
 
 Ao final, mostre o conteúdo das três listas geradas.
 '''
+numeros = []
+pares = []
+impares = []
+while True:
+    print("-="*15)
+    n = int(input("Digite um número: "))
+    numeros.append(n)
+    print("-="*15)
+    while True:
+        continuar = str(input("Deseja continuar [S|N]: ")).strip().upper()
+        if continuar not in 'SN':
+            continue
+        elif continuar in 'S':
+            break
+        elif continuar in 'N':
+            break
+    if continuar in 'N':
+        break
+for num in numeros:
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+print(f"Lista de números informados: {numeros}")
+print(f"Lista de números pares: {pares}")
+print(f"Lista de números ímpares: {impares}")
