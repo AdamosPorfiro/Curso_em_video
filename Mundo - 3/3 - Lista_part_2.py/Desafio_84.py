6864442fa8672668fc788a25f405e5ec4655159e
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-19 20:24:34 -0300

    Desafios.py

---
+++ Mundo - 3/3 - Lista_part_2.py/Desafio_84.py
@@ -0,0 +1,9 @@
+'''
+Faça um programa que leia o nome e peso de varias pessoas e guarde tudo em uma lista. No final mostre
+
+a - Quantas pessoas foram cadastradas
+
+b - Uma listagem com as pessoas mais pesadas
+
+c - Uma listagem com as pessoas mais leve
+'''
