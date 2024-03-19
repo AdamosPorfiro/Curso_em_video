5a1f7bc93898e75acb0543cc7734d048c76c845d
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-19 17:16:16 -0300

    Desafio_83.py

--- Mundo - 3/2 - Lista_part_1.py/Desafio_83.py
+++ Mundo - 3/2 - Lista_part_1.py/Desafio_83.py
@@ -13,3 +13,35 @@ try:
 except(SyntaxError, TypeError):
     print('Expressão Invalida!')
 '''
+
+conchetes_abre = []
+conchetes_fecha = []
+expressão = str(input("Digite uma espressão matemática: "))
+
+#Criamos um FOR para passar por cada conchete que abre e fecha para armazena-los
+for c in expressão:
+    if c in '(':
+        conchetes_abre.append(c)
+    if c in ')':
+        conchetes_fecha.append(c)
+
+#A cada vez que ele passa o laço ele remove o último conhecete que abre
+    if len(conchetes_abre) >= 1 and len(conchetes_fecha) >= 1:
+        conchetes_abre.pop()
+        conchetes_fecha.pop()
+
+#Verifica as listas para confirmar se os conchetes estão corretos
+if len(conchetes_abre) == 0 and len(conchetes_fecha) == 0:
+        print("Expressão válida!")
+        print(conchetes_fecha)
+else:
+    print("Expressão inválida!")
+    print(conchetes_fecha)
+"""
+        if conchetes == 0:
+            print("Expressão válida!")
+            break
+        else:
+            print("Expressão inválida")
+            break
+        """
