171b1fc1bb0a7ad1e2fb749728a5bf4079f4605d
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-21 17:23:39 -0300

    Desafio_86_incompleto.py

--- Mundo - 3/3 - Lista_part_2.py/Desafio_86.py
+++ Mundo - 3/3 - Lista_part_2.py/Desafio_86.py
@@ -2,3 +2,17 @@
 Crie um programa que crie uma matriz de dimensão 3x3 e preencha com valores
 lidos pelo teclado, no final, mostre a matriz na tela. COm a formatação correta.
 '''
+matriz_1 = [[],[],[]]
+matriz_2 = [[],[],[]]
+matriz_3 = [[],[],[]]
+
+for c in range(3):
+    numero = int(input("Digite um valor: "))
+    if matriz_1[0] == []:
+        matriz_1[0] = numero
+    elif matriz_1[1] == list([]):
+        matriz_1[1] = numero
+    elif matriz_1[2] == list([]):
+        matriz_1[2] = numero
+print(f"{matriz_1}")
+               
