fce5c9c0af8702279f8de4b9724673aa802d990b
AdamosPorfiro <adamos.contato@gmail.com>
2024-03-20 21:43:06 -0300

    Desafio_85_incompleto.py

--- Mundo - 3/3 - Lista_part_2.py/Desafio_85.py
+++ Mundo - 3/3 - Lista_part_2.py/Desafio_85.py
@@ -4,14 +4,11 @@ cadastre-os em uma lista única que mantenha separados os valores pares e
 ímpares. No final mostre os valores pares e ímpares em ordem crescente.
 '''
 lista_de_numeros = []
-pares = []
-ímpares = []
+pares_impares = []
 for c in range (0,7):
     lista_de_numeros.append(int(input("Digite um valor: ")))
-    for c in lista_de_numeros:
-        if c % 2 == 0:
-            pares.append(c)
-        else:
-            ímpares.append(c)
+    pares_impares.append(lista_de_numeros[:])
     lista_de_numeros.clear()
-print(f"Pares: {sorted(pares)}\nImpares: {sorted(ímpares)}")
+print(pares_impares)
+    
+#print(f"Pares: {sorted(pares_impares)}\nImpares: {sorted(pares_impares)}")
