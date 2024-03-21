ffc0f27f7f288fe778b45a3cda70e7bf306212e9
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-21 15:12:02 -0300

    Desafio_85.py

--- Mundo - 3/3 - Lista_part_2.py/Desafio_85.py
+++ Mundo - 3/3 - Lista_part_2.py/Desafio_85.py
@@ -3,12 +3,16 @@ Crie um programa onde o usuario podera digitar sete valores númericos e
 cadastre-os em uma lista única que mantenha separados os valores pares e
 ímpares. No final mostre os valores pares e ímpares em ordem crescente.
 '''
-lista_de_numeros = []
-pares_impares = []
-for c in range (0,7):
-    lista_de_numeros.append(int(input("Digite um valor: ")))
-    pares_impares.append(lista_de_numeros[:])
-    lista_de_numeros.clear()
-print(pares_impares)
-    
-#print(f"Pares: {sorted(pares_impares)}\nImpares: {sorted(pares_impares)}")
+
+pares_impares = [[], []] #Criar uma lista única com sublistas que são listas dentro de listas.
+for c in range (7):
+    numero = int(input("Digite um valor: "))
+    if numero % 2 == 0:
+        pares_impares[0].append(numero)
+    else:
+        pares_impares[1].append(numero)
+#----------------------------------------------------
+print("-="*10)
+print(f"Números pares são: {sorted(pares_impares[0])}")
+print(f"Números ímpares são: {sorted(pares_impares[1])}")
+print("-="*10)
