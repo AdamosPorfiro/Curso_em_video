b1855ff516075da1afea88ef894151e62b610c16
Adamos Porfiro <adamos.contato@gmail.com>
2024-03-23 16:39:14 -0300

    Desafio_88_incompleto.py

--- Mundo - 3/3 - Lista_part_2.py/Desafio_88.py
+++ Mundo - 3/3 - Lista_part_2.py/Desafio_88.py
@@ -2,4 +2,28 @@
 Faça um programa que ajuda um jogador da mega sena a criar palpites. O programa
 vai perguntar quantos jogos serão gerados e vai sortear 6 números entre 1 e 60
 para cada jogo, cadastrando tudo em uma lista composta.
+
+5 Q's
+
+1 - Quais são os dados de entrada?
+
+- Quantidade de jogos a ser gerado;
+- Para cada jogo vamos gerar 6 números
+
+2 - O que devo fazer com esses dados
+
+- Vamos gerar 6 números para cada jogo definido pelo usuario
+
+3 - Quais são as restrições do problema?
+
+- Serão 6 números entre 1 e 60 para cada jogo.
+
+4 - Qual é o resultado esperado?
+
+- O usuario deverá informar quantos jogos deseja gerar;
+- Para cada jogo gerado, serão sorteados 6 números diferentes entre 1 á 60;
+- No final vamos exibir o resultado para o usuario;
+
+5 - Qual a sequência de passos para alcançar o resultado?
+
 '''
