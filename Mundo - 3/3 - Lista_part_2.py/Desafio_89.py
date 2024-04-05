6f336181f583244a25f3b9fb026e1d539d421b5a
AdamosPorfiro <adamos.contato@gmail.com>
2024-04-04 22:12:56 -0300

    Desafio_89.py

--- Mundo - 3/3 - Lista_part_2.py/Desafio_89.py
+++ Mundo - 3/3 - Lista_part_2.py/Desafio_89.py
@@ -18,10 +18,27 @@ while True:
 
 ficha = list()
 
-nome = str(input("Nome: "))
-nota_1 = float(input("1° Nota: "))
-nota_2 = float(input("2° Nota: "))
-media = 2*(nota_1+nota_2)
-ficha.append((nome,(nota_1, nota_2), media))
+while True:
+    nome = str(input("Nome: "))
+    nota_1 = float(input("1° Nota: "))
+    nota_2 = float(input("2° Nota: "))
+    media = (nota_1 + nota_2) / 2
+    ficha.append([nome,[nota_1, nota_2], media])
+    resp = str(input("Quer continuar [S|N]: ")).upper().strip()
+    if resp == "N":
+        break
+print("-="*20)
+print(f"{"No.":<8}{"NOME":12}{"NOTA":>2}")
 
-print(ficha)
+for p, a in enumerate(ficha):
+    print(f"{p+1:<8}{a[0]:<12}{a[2]:>2.1f}")
+
+while True:
+    print("-"*35)
+    opc = int(input("Mostrar notas de algum aluno? [999 - para sair]: "))
+    if opc == 999:
+        print("FINALIZANDO...")
+        break
+    elif opc <= len(ficha):
+        print(f"Notas de {ficha[opc-1][0]} são {ficha[opc-1][1]}")
+print("<<<< VOLTE SEMPRE >>>>")
