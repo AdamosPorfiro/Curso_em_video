acc1247161b2bea0a530e3c1f7b23ca2d558612c
AdamosPorfiro <adamos.contato@gmail.com>
2024-04-07 19:19:16 -0300

    Desafio_90.py

--- Mundo - 3/4 - Dicionário/Desafio_90.py
+++ Mundo - 3/4 - Dicionário/Desafio_90.py
@@ -1,5 +1,19 @@
 """
-Faça um programa que leia nome e méda de um aluno,
-guardand também a situação em um dicionário. No
+Faça um programa que leia nome e média de um aluno,
+guardando também a situação em um dicionário. No
 final mostre o conteúdo da estrutura na tela.
 """
+situacao = {}
+situacao["nome"] = str(input("Nome: ")).capitalize()
+situacao["media"] = float(input("Média: "))
+print()
+print(f"="*20)
+print(f"{"Nome":<10}{"Média":>8}")
+print(f"{situacao['nome']:<10}{situacao['media']:>7}")
+print(f"="*20)
+"""
+print(f"{situacao.values()}")#Exibir apenas os valores dentro do dicionario
+print(f"{situacao.keys()}")#Exibir apenas o nome das chaves
+print(f"{situacao.items()}")#Exibir os dois "values" e "keys"
+"""
+
