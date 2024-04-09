acec6a1c55a5f8350684ed2e4e07df0636207cee
AdamosPorfiro <adamos.contato@gmail.com>
2024-04-08 21:34:00 -0300

    Desafio_91.py

--- Mundo - 3/4 - Dicionário/Desafio_91.py
+++ Mundo - 3/4 - Dicionário/Desafio_91.py
@@ -7,11 +7,19 @@ sabendo que o vencedor tirou o maior número no dado.
 from random import randint
 resultado_inicial = dict()
 resultado_oficial = list()
+outra_lista = list()
 
 #Programa que guarda o resultado em um dicionario e gera uma copia para um lista organizada
 for c in range(4):
     resultado_inicial["numero"] = randint(1,6)
     resultado_oficial.append(resultado_inicial.copy())
+resultado_inicial.clear()
+
 for r in resultado_oficial:
     for k,v in r.items():
-        print(f"{v}")
+        outra_lista.append(v)
+        resultado_inicial[k] = outra_lista
+        
+print(f"{outra_lista}")
+
+        
