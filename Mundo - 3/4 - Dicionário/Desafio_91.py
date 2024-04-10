44e8c101da7be9dabdd0a4626680f07e1d066acc
AdamosPorfiro <adamos.contato@gmail.com>
2024-04-09 21:00:23 -0300

    Desafio_91.py

--- Mundo - 3/4 - Dicionário/Desafio_91.py
+++ Mundo - 3/4 - Dicionário/Desafio_91.py
@@ -5,21 +5,27 @@ resultados em um dicionario. No final coloque esse dicionário em ordem
 sabendo que o vencedor tirou o maior número no dado.
 """
 from random import randint
+from time import sleep
 resultado_inicial = dict()
 resultado_oficial = list()
 outra_lista = list()
+maior = 0
 
 #Programa que guarda o resultado em um dicionario e gera uma copia para um lista organizada
 for c in range(4):
-    resultado_inicial["numero"] = randint(1,6)
+    print(f"{c+1}°Jogador...rolando dado")
+    resultado_inicial["numero"] = randint(1,6) #Gerar os numeros e armazenar em um dicionario
+    print(f"{resultado_inicial.values()}")
+    sleep(0.7)
     resultado_oficial.append(resultado_inicial.copy())
 resultado_inicial.clear()
 
-for r in resultado_oficial:
+for r in resultado_oficial: #Entro dentro da lista que existe os dicionarios
     for k,v in r.items():
+        if v > maior:
+            maior = v
         outra_lista.append(v)
-        resultado_inicial[k] = outra_lista
-        
-print(f"{outra_lista}")
+print(f"{sorted(outra_lista)}")
+print(f"O vencedor jogou: {maior}")
 
         
