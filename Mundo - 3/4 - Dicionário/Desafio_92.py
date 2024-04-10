96497ea3d1e2437c6861ffe3cf2999263b335b53
Adamos Porfiro <adamos.contato@gmail.com>
2024-04-10 18:10:15 -0300

    .py

--- Mundo - 3/4 - Dicionário/Desafio_92.py
+++ Mundo - 3/4 - Dicionário/Desafio_92.py
@@ -1,10 +1,18 @@
 """
 Crie um programa que leia nome, ano de nascimento e carteira
-de trabalho e cadastre-os(com idade) em um dicionario se for acaso a ctps
-se por acaso a CTPS for diferente de ZERO, o dicionario recebera
+de trabalho e cadastre-os(com idade) em um dicionario. 
+Se por acaso a CTPS for diferente de ZERO, o dicionario recebera
 também o ano de contratação e o salário.
 Calcule e acrescente, além da idade, com quantos anos
 a pessoa vai se aposentar.
 
-Considere para se aposentar são 35 anos de contribuição
+Considere que para se aposentar são 35 anos de contribuição
 """
+dados_ = dict()
+dados_copy = list()
+
+while True:
+    dados_["nome"] = str(input("Nome: "))
+    dados_["ano_nascimento"] = int(input("Ano de nascimento: "))
+    dados_["ctps"] = str(input("Possui carteira de trabalho assinada [S/N]"))
+
