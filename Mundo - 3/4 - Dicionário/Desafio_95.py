dead1270a3ffce10af75a05a424c9a28847dd14c
AdamosPorfiro <adamos.contato@gmail.com>
2024-05-04 20:08:44 -0300

    Desafio_95.py

--- Mundo - 3/4 - Dicionário/Desafio_95.py
+++ Mundo - 3/4 - Dicionário/Desafio_95.py
@@ -3,53 +3,27 @@ Aprimore o desafio 093 para que ele funcione com vários jogadores,
 incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
 """
 
-dados_jogador = dict()
-dados_finais_jogador = list()
-quantidade_de_gols = 0
+Jogador = dict()
+Partidas = list()
 
-while True:
-    dados_jogador["nome"] = str(input("Nome: "))
-    dados_jogador["Numero_partidas"] = int(input("Quantidade de partidas: "))
-    dados_finais_jogador.append(dados_jogador.copy())
-    dados_jogador.clear()
 
-    resp = str(input("Continuar? [S/N]: ")).upper().strip()
-    if resp == "N":
-        break
+Jogador["nome"] = str(input("Nome jogador: "))
+tot = int(input(f"Quantas partidas o jogador {Jogador["nome"]} jogou? "))
+for c in range(0, tot):
+    Partidas.append(int(input(f"Quantos gols na partida {c+1}°? ")))
+Jogador["Gols"] = Partidas[:]
+Jogador["Total"]= sum(Partidas)
 
-for indice,valor in enumerate(dados_finais_jogador):
-    dados_jogador[f""]
-    print(f"{dados_finais_jogador[indice]["Numero_partidas"]}")
-"""
-    for c in range(dados_finais_jogador[0]["Numero_partidas"]):
-        dados_jogador[f"qtd_gols_partida"] = int(input(f"Quantos gols foram feitos na {c+1}° partida: "))
-        dados_finais_jogador.append(dados_jogador.copy())
-        dados_jogador.clear()
+print("-="*30)
+print(f"{Jogador}")
+print("-="*30)
 
-    resp = str(input("Continuar? [S/N]: ")).upper().strip()
-    if resp == "N":
-        break
+for k,v in Jogador.items():
+    print(f"O campo {k} tem o valor {v}")
+print("-="*30)
 
-for r in dados_finais_jogador:
-    for k, v in r.items():
-        if k == "qtd_gols_partida":
-            quantidade_de_gols += v
-dados_jogador["total_de_gols"] = quantidade_de_gols
-dados_finais_jogador.append(dados_jogador.copy())
-dados_jogador.clear()
-print("="*30)
-print("Dados do jogador")
-print("="*30)
-for r in dados_finais_jogador:
-    for k,v in r.items():
-        if k == "nome":
-            print(f"Nome: {v}")
-        if k == "Numero_partidas":
-            print(f"Total de partidas jogadas: {v}")
+print(f"O jogador {Jogador["nome"]} jogou {len(Jogador["Gols"])} partidas")
+for i, v in enumerate(Jogador["Gols"]):
+    print(f"    => Na partida {i+1} fez {v} gols.")
+print(f"Foi um total de {Jogador["Total"]} gols.")
 
-for c, r in enumerate(dados_finais_jogador[1:3]):
-        for k,v in r.items():
-            if k == "qtd_gols_partida":
-                print(f"Quantidade de gols da {c+1}° partida: {v}")
-print(f"Total de gols: {dados_finais_jogador[3]["total_de_gols"]}")
-"""
