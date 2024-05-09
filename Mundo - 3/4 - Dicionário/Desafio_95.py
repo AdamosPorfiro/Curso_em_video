5b938fd5bfcf5d0efb93c7b420c6804eae404402
AdamosPorfiro <adamos.contato@gmail.com>
2024-05-08 21:03:59 -0300

    Desafio_95.py

--- Mundo - 3/4 - Dicionário/Desafio_95.py
+++ Mundo - 3/4 - Dicionário/Desafio_95.py
@@ -8,9 +8,11 @@ Partidas = list()
 Jogadores = list()
 
 while True:
+    Jogador.clear()
     Jogador["nome"] = str(input("Nome jogador: "))
 #--------------------------------------------------------------------------------------
     while True:
+        Partidas.clear()
         tot = str(input(f"Quantas partidas o jogador {Jogador["nome"]} jogou? "))
         if tot.isdigit():
             tot = int(tot)
@@ -27,19 +29,25 @@ while True:
     Jogador["Gols"] = Partidas[:]
     Jogador["Total"]= sum(Partidas)
     Jogadores.append(Jogador.copy())
-    Jogador.clear()
 #--------------------------------------------------------------------------------------
-    print("-="*30)
-    resp = str(input("Deseja continuar? [ S | N ]: ")).upper().strip()
-    print("-="*30)
-    if resp == "N":
+    while True:
+        print("-="*30)
+        resp = str(input("Deseja continuar? [ S | N ]: ")).upper().strip()
+        print("-="*30)
+        if resp not in 'SN':
+            continue
+        elif resp in 'SN':
+            break
+    if resp == 'N':
         break
-
 #--------------------------------------------------------------------------------------  
 print("-"*50)
-print(f"{"cod"}{"|":>4}{"nome":>6}{"|":>6}{"gols":>9}{"|":>4}{"total":>10}")
+print(f"{"cod":>4}{"|":>2}{"nome":>6}{"|":>4}{"gols":>7}{"|":>4}{"total":>10}")
 print("-"*50)
+
 for indice,valor in enumerate(Jogadores):
-    print(f"{indice}",end='')
+    print(f"{indice:>4}  ",end='')
     for v in valor.values():
-        print(f"{str(v):>12}",end='')
+        print(f"{str(v):<15}",end='')
+    print()
+print("-"*50)
